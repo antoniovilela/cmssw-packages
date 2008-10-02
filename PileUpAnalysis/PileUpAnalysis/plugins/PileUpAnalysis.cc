6bcdcd714be1da2876f520b8317b60dbd74195e1
antoniov <>
2008-10-02 17:01:29 +0000

    start adding track association

--- PileUpAnalysis/PileUpAnalysis/plugins/PileUpAnalysis.cc
+++ PileUpAnalysis/PileUpAnalysis/plugins/PileUpAnalysis.cc
@@ -3,6 +3,9 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
+class InputTag;
+class TrackAssociatorBase;
+
 class PileUpAnalysis  : public edm::EDAnalyzer {
  public:
 
@@ -11,34 +14,46 @@ class PileUpAnalysis  : public edm::EDAnalyzer {
   virtual ~PileUpAnalysis(){}
 
   virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
-
+  virtual void beginJob(const edm::EventSetup & setup);
  private:
-  edm::ParameterSet conf_;
-
+  edm::InputTag tracksTag_;
+  edm::InputTag trackAssociatorTag_;
+  TrackAssociatorBase* associatorByHits_;
+  
 };
 
 #endif
 
 #include "FWCore/Framework/interface/EDAnalyzer.h"
-#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Event.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
 #include "DataFormats/Provenance/interface/EventID.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 
-#include "DataFormats/Common/interface/EDProduct.h"
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-
-#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
-#include "SimDataFormats/CrossingFrame/interface/MixCollection.h"
-#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
+//#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
+//#include "SimDataFormats/CrossingFrame/interface/CrossingFrame.h"
+//#include "SimDataFormats/CrossingFrame/interface/MixCollection.h"
 #include "SimDataFormats/EncodedEventId/interface/EncodedEventId.h"
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingParticleFwd.h"
 #include "SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h"
-
-#include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 #include "SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfo.h"
 
+#include "SimTracker/TrackAssociation/interface/TrackAssociatorBase.h"
+#include "SimTracker/Records/interface/TrackAssociatorRecord.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+
+#include <vector>
 #include <map>
 
 typedef edm::RefVector< std::vector<TrackingParticle> > TrackingParticleContainer;
@@ -53,15 +68,22 @@ typedef TrackingVertex::g4v_iterator                     g4v_iterator;
 typedef std::map<std::pair<int,int>,unsigned int> TrackMultMap;
 
 PileUpAnalysis::PileUpAnalysis(const edm::ParameterSet& conf){
-  conf_ = conf;
+  tracksTag_ = conf.getParameter<edm::InputTag>("TracksTag");
+}
+
+void PileUpAnalysis::beginJob(const edm::EventSetup& setup) {
+  edm::ESHandle<MagneticField> theMF;
+  setup.get<IdealMagneticFieldRecord>().get(theMF);
+  edm::ESHandle<TrackAssociatorBase> theHitsAssociator;
+  setup.get<TrackAssociatorRecord>().get("TrackAssociatorByHits",theHitsAssociator);
+  associatorByHits_ = (TrackAssociatorBase *) theHitsAssociator.product();
 }
 
 void PileUpAnalysis::analyze(const edm::Event& event, const edm::EventSetup& c){
-  using namespace std;
 
-  edm::Handle<TrackingParticleCollection> elecPH;
+  /*edm::Handle<TrackingParticleCollection> elecPH;
   edm::Handle<TrackingParticleCollection> rawPH;
-  edm::Handle<TrackingVertexCollection>   rawVH;
+  edm::Handle<TrackingVertexCollection>   rawVH;*/
   edm::Handle<TrackingParticleCollection> mergedPH;
   edm::Handle<TrackingVertexCollection>   mergedVH;
 
@@ -84,7 +106,7 @@ void PileUpAnalysis::analyze(const edm::Event& event, const edm::EventSetup& c){
   playbackInfo->getEventStartInfo(ids,fileNrs,nrEvents,s);
 
   for(unsigned int ibunch = 0; ibunch < nrEvents.size(); ++ibunch){
-    std::cout << " Number of added pile-up's for bunch index " << ibunch << ": " << nrEvents[ibunch] << std::endl; 
+    edm::LogVerbatim("Analysis") << " Number of added pile-up's for bunch index " << ibunch << ": " << nrEvents[ibunch]; 
   }
 
   /*cout << endl << "Dump of initial vertices: " << endl;
@@ -112,7 +134,7 @@ void PileUpAnalysis::analyze(const edm::Event& event, const edm::EventSetup& c){
   }*/
 
   // Build map giving nr. of tracks vs (bx,evt)
-  cout << endl << "Dump of merged tracks: " << endl;
+  edm::LogVerbatim("Analysis") << " Tracking particles summary:";
   TrackMultMap trackMultMap;
   for (TrackingParticleCollection::const_iterator iTrack = mergedPH->begin(); iTrack != mergedPH->end(); ++iTrack) {
     //cout << *iTrack;
@@ -127,8 +149,38 @@ void PileUpAnalysis::analyze(const edm::Event& event, const edm::EventSetup& c){
 
   // Print out track mult.
   for(TrackMultMap::const_iterator it = trackMultMap.begin(); it != trackMultMap.end(); ++it){
-    std::cout << " Number of tracks in (bx,evt) (" << it->first.first << "," << it->first.second << "): " << it->second << std::endl; 
+     edm::LogVerbatim("Analysis") << " Number of tracks in (bx,evt) (" << it->first.first << "," << it->first.second << "): " << it->second; 
   }
+
+  // Add TrackAssociator info 
+
+  // RecoToSim
+  edm::Handle<edm::View<reco::Track> > trackCollectionH;
+  event.getByLabel(tracksTag_,trackCollectionH);
+  const edm::View<reco::Track> tC = *(trackCollectionH.product());
+
+  /*edm::Handle<reco::RecoToSimCollection> recoToSimCollectionH;
+  event.getByLabel(trackAssociatorTag_,recoToSimCollectionH);
+
+  reco::RecoToSimCollection& recoToSim = *recoToSimCollectionH.product();*/
+
+  reco::RecoToSimCollection recoToSim = associatorByHits_->associateRecoToSim(trackCollectionH,mergedPH,&event);
+
+  for(edm::View<reco::Track>::size_type i=0; i < tC.size(); ++i) {
+    edm::RefToBase<reco::Track> track(trackCollectionH, i);
+    if(recoToSim.find(track) != recoToSim.end()){
+        std::vector<std::pair<TrackingParticleRef, double> > tp = recoToSim[track];
+	edm::LogVerbatim("Analysis") << "Reco Track pT: "  << track->pt() 
+	                             <<  " matched to " << tp.size() << " MC Tracks";
+	for (std::vector<std::pair<TrackingParticleRef, double> >::const_iterator it = tp.begin(); 
+	     it != tp.end(); ++it) {
+	     TrackingParticleRef tpr = it->first;
+	     double assocChi2 = it->second;
+	     edm::LogVerbatim("Analysis") << "\t\tMCTrack " << tpr.index() << " pT: " << tpr->pt() << " NShared: " << assocChi2;
+	}
+     } else edm::LogWarning("Analysis") << "->   Track pT: " << track->pt() <<  " matched to 0  MC Tracks";
+  }
+  
 }
 
 DEFINE_SEAL_MODULE();
--- PileUpAnalysis/PileUpAnalysis/test/PileUpAnalysis_cfg.py
+++ PileUpAnalysis/PileUpAnalysis/test/PileUpAnalysis_cfg.py
@@ -1,23 +1,34 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("TEST")
+process = cms.Process("Analysis")
 # keep the logging output to a nice level ###
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 # Tracking Truth and mixing module, if needed
 #process.load("SimGeneral.MixingModule.mixNoPU_cfi")
 
-process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
+#process.load("SimGeneral.TrackingAnalysis.trackingParticles_cfi")
+
+process.load("Configuration.StandardSequences.MagneticField_cff") 
+process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
+#process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/tmp/antoniov/PYTHIA6_EWK_Wmunu_10TeV_InitialLumPU_cff_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root')
+    fileNames = cms.untracked.vstring('file:/tmp/antoniov/PYTHIA6_EWK_Wmunu_10TeV_InitialLumPU_cff_py_RAW2DIGI_RECO.root')
 )
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
-process.myTest = cms.EDFilter("PileUpAnalysis")
+process.pileupanalysis = cms.EDFilter("PileUpAnalysis",
+                              TracksTag = cms.InputTag("generalTracks"),
+			      TrackAssociatorTag = cms.InputTag("trackingParticleRecoTrackAsssociation")	 
+)
 
-#process.p = cms.EndPath(process.mix*process.trackingParticles*process.myTest)
+#process.p = cms.EndPath(process.mix*process.trackingParticles*process.pileupanalysis)
 
-process.p = cms.Path(process.trackingParticles*process.myTest)
+#process.p = cms.Path(process.trackingParticles*process.pileupanalysis)
+
+#process.p = cms.Path(process.trackingParticleRecoTrackAsssociation*process.pileupanalysis)
+
+process.p = cms.Path(process.pileupanalysis)
