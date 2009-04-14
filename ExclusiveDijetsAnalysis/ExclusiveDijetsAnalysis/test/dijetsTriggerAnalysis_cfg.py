f0747f59276d157a7d1915feebfbfcfb6eb8dad4
antoniov <>
2009-04-14 09:21:25 +0000

    add filter for L1 info

--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/dijetsTriggerAnalysis_cfg.py
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/dijetsTriggerAnalysis_cfg.py
@@ -4,18 +4,31 @@ process = cms.Process("Analysis")
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.MessageLogger.debugModules = cms.untracked.vstring('analysis')
-process.MessageLogger.cerr.threshold = 'DEBUG'
-#process.MessageLogger.cerr.INFO.limit = -1
+process.MessageLogger.cerr.threshold = 'INFO'
+process.MessageLogger.cerr.INFO.limit = -1
 
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/tmp/antoniov/QCDpt30_Summer08_IDEAL_V11_redigi_v1_RECO_345194AC-4EE3-DD11-9EF6-001D0967D634.root')
+    fileNames = cms.untracked.vstring('file:/tmp/antoniov/ExHuME_CEPDijetsGG_M100_10TeV_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_PU_1.root')
+    #fileNames = cms.untracked.vstring('file:/tmp/antoniov/ExHuME_CEPDijetsGG_M100_10TeV_cff_py_RAW2DIGI_RECO_1.root')
 )
 
 #process.load("ExclusiveDijetsAnalysis.ExclusiveDijetsAnalysis.exclusiveDijetsHLTPaths_cfi")
+
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+#process.load('Configuration/StandardSequences/MagneticField_38T_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'IDEAL_V11::All'
+
+process.load('Configuration/StandardSequences/L1TriggerDefaultMenu_cff')
+#from HLTrigger.Configuration.HLT_2E30_cff import hltL1sDiJetAve30,HLTRecoJetMETSequence,hltdijetave30
+#process.hltL1sDiJetAve30 = hltL1sDiJetAve30
+#process.HLTRecoJetMETSequence = HLTRecoJetMETSequence
+#process.hltdijetave30 = hltdijetave30
+process.load('HLTrigger.Configuration.HLT_2E30_cff')
 
 process.load("DiffractiveForwardAnalysis.SingleDiffractiveWAnalysis.pileUpInfo_cfi")
 from DiffractiveForwardAnalysis.SingleDiffractiveWAnalysis.pileUpNumberFilter_cfi import *
@@ -23,8 +36,8 @@ process.filter0PU = pileUpNumberFilter.clone(NumberOfPileUpEvents = 0)
 
 process.analysis = cms.EDAnalyzer("DijetsTriggerAnalyzer",
   #bit counts per ring
-  HFRingETSumThreshold = cms.int32(1),
-  AccessL1GctHFRingEtSums = cms.untracked.bool(False)
+  HFRingETSumThreshold = cms.int32(0),
+  AccessL1GctHFRingEtSums = cms.untracked.bool(True)
 )
 
 process.analysis0PU = process.analysis.clone()
@@ -34,4 +47,13 @@ process.TFileService = cms.Service("TFileService",
 )
 
 process.analysis0PU_seq = cms.Sequence(process.pileUpInfo*process.filter0PU*process.analysis0PU)
+
+#process.hlt_step = cms.Path(process.hltL1sDiJetAve30 + process.HLTRecoJetMETSequence + process.hltdijetave30)
+
+#process.hlt = cms.Sequence(process.exclusiveDijetsHLTFilter)
+#process.analysis_step = cms.Path(process.hlt+process.analysis+process.analysis0PU_seq)
+
 process.analysis_step = cms.Path(process.analysis+process.analysis0PU_seq)
+
+#process.schedule = cms.Schedule(process.HLTriggerFirstPath,process.HLT_DiJetAve30,process.analysis_step)
+process.schedule = cms.Schedule(process.HLT_DiJetAve30,process.analysis_step)
--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/dijetsTriggerAnalysis_noPU_cfg.py
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/dijetsTriggerAnalysis_noPU_cfg.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Analysis")
 
 process.load('FWCore.MessageService.MessageLogger_cfi')
-process.MessageLogger.debugModules = cms.untracked.vstring('analysis')
+process.MessageLogger.debugModules = cms.untracked.vstring('l1filter')
 process.MessageLogger.cerr.threshold = 'DEBUG'
 #process.MessageLogger.cerr.INFO.limit = -1
 
@@ -15,14 +15,31 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:/tmp/antoniov/QCDpt30_Summer08_IDEAL_V11_redigi_v1_RECO_345194AC-4EE3-DD11-9EF6-001D0967D634.root')
 )
 
+process.load("ExclusiveDijetsAnalysis.ExclusiveDijetsAnalysis.exclusiveDijetsHLTPaths_cfi")
+
 process.analysis = cms.EDAnalyzer("DijetsTriggerAnalyzer",
   #bit counts per ring
-  HFRingETSumThreshold = cms.int32(1),
+  HFRingETSumThreshold = cms.int32(0),
   AccessL1GctHFRingEtSums = cms.untracked.bool(False)
 )
+
+process.analysisHLT = process.analysis.clone()
+
+process.l1filter = cms.EDFilter("L1TriggerTestFilter",
+  HFRingETSumThreshold = cms.int32(0),
+  L1TriggerNames = cms.vstring("L1_SingleJet30"),
+  AccessL1GctHFRingEtSums = cms.untracked.bool(False) 
+)
+
+process.l1filterHLT = process.l1filter.clone()
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("analysisDijetsTrigger_histos.root")
 )
 
+process.hlt = cms.Sequence(process.exclusiveDijetsHLTFilter)
 process.analysis_step = cms.Path(process.analysis)
+process.analysis_step_hlt = cms.Path(process.hlt + process.analysisHLT)
+
+process.filter = cms.Path(process.l1filter)
+process.filter_hlt = cms.Path(process.hlt + process.l1filterHLT)
