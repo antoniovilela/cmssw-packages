64d5d9059dd3ecddd1825823868a217ff0617ccb
antoniov <>
2009-07-06 16:04:10 +0000

    update

--- CalibMuon/DTCalibration/Workflow/workflow.py
+++ CalibMuon/DTCalibration/Workflow/workflow.py
@@ -199,10 +199,19 @@ def haddInCastor(castor_dir,result_file,type = 'root'):
     if not castor_dir: raise ValueError,'Please specify valid castor dir'
     if not result_file: raise ValueError,'Please specify valid output file name'
 
-    cmd = 'hadd %s `./listfilesCastor %s | grep %s`'%(result_file,castor_dir,type)
-
-    print "Running",cmd
-    os.system(cmd)
+    #cmd = 'hadd %s `./listfilesCastor %s | grep %s`'%(result_file,castor_dir,type)
+    #print "Running",cmd
+    #os.system(cmd)
+    from subprocess import Popen,PIPE,call
+    p1 = Popen(['nsls',castor_dir],stdout=PIPE)
+    p2 = Popen(['grep',type],stdin=p1.stdout,stdout=PIPE)
+    files = [castor_dir + "/" + item[:-1] for item in p2.stdout]
+    p2.stdout.close()
+    cmd = ['hadd',result_file]
+    cmd.extend(files)
+    #print cmd
+    retcode = call(cmd)
+    return retcode
 
 def setGridEnv(cmssw_dir):
     cwd = os.getcwd()
