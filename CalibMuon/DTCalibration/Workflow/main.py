262abfc0329938331bf18532b156797f1d404a5e
antoniov <>
2009-08-24 08:56:56 +0000

    update

--- CalibMuon/DTCalibration/Workflow/main.py
+++ CalibMuon/DTCalibration/Workflow/main.py
@@ -7,6 +7,7 @@ from CrabWatch import CrabWatch
 from workflow import haddInCastor,crabWatch,getOutput
 import sys,os,time
 
+"""
 #FIXME: This should come from some configuration module
 castor_prefix = '/castor/cern.ch/cms/store/caf/user/antoniov/'
  
@@ -14,15 +15,16 @@ def_crab_opts = {'DATASETPATH':'/StreamExpress/CRAFT09-MuAlCalIsolatedMu-v1/ALCA
                  'EMAIL':'vilela@to.infn.it'}
 
 def_pset_opts = {'GLOBALTAG':'GR09_P_V1::All'}
+"""
 
-def runTtrigProd(run,runselection,trial,result_dir):
+def runTtrigProd(run,runselection,trial,result_dir,config):
 
     crab_opts = {'RUNSELECTION':runselection,
                  'USERDIRCAF':'TTRIGCalibration/Production/Run' + str(run) + '/v' + str(trial)}
-    crab_opts.update(def_crab_opts)
+    crab_opts.update(config.crab_opts)
 
     pset_opts = {'MUDIGILABEL':'muonDTDigis'}
-    pset_opts.update(def_pset_opts)
+    pset_opts.update(config.pset_opts)
 
     dtTtrigProd = DTTTrigProd(run,crab_opts,pset_opts) 
     project_prod = dtTtrigProd.run()
@@ -34,29 +36,29 @@ def runTtrigProd(run,runselection,trial,result_dir):
     crabProd.start()
     crabProd.join()
 
-    castor_dir = castor_prefix + dtTtrigProd.crab_opts['USERDIRCAF']
+    castor_dir = config.castor_prefix + dtTtrigProd.crab_opts['USERDIRCAF']
 
-    result_file = result_dir + '/DTTimeBoxes_%s.root'%run
+    result_file = config.result_dir + '/DTTimeBoxes_%s.root'%run
     haddInCastor(castor_dir,result_file)
 
-def runTtrigCorrFirst(run,result_dir):
-    dtTtrigCorrFirst = DTTTrigCorrFirst(run,result_dir)  
+def runTtrigCorrFirst(run,config):
+    dtTtrigCorrFirst = DTTTrigCorrFirst(run,config.result_dir)  
     dtTtrigCorrFirst.run()
 
     print "Finished processing:"
     for pset in dtTtrigCorrFirst.configs: print "--->",pset
 
-def runTtrigValid(run,runselection,trial,input_file,result_dir):
+def runTtrigValid(run,runselection,trial,input_file,config):
     #ttrig_second_db = os.path.abspath(result_dir + '/' + 'ttrig_second_' + run + '.db')
 
     crab_opts = {'RUNSELECTION':runselection,
                  'USERDIRCAF':'TTRIGCalibration/Validation/First/Run' + str(run) + '/v' + str(trial),
                  'INPUTFILE':input_file}
 
-    crab_opts.update(def_crab_opts)
+    crab_opts.update(config.crab_opts)
  
     pset_opts = {'INPUTFILE':input_file.split('/')[-1]}
-    pset_opts.update(def_pset_opts)
+    pset_opts.update(config.pset_opts)
 
     dtTtrigValid = DTTTrigValid(run,crab_opts,pset_opts) 
     project_valid_first = dtTtrigValid.run()
@@ -68,28 +70,28 @@ def runTtrigValid(run,runselection,trial,input_file,result_dir):
     crabValidFirst.start()
     crabValidFirst.join()
 
-    result_file = result_dir + '/DTkFactValidation_%s.root'%run
-    castor_dir = castor_prefix + dtTtrigValid.crab_opts['USERDIRCAF']
+    result_file = config.result_dir + '/DTkFactValidation_%s.root'%run
+    castor_dir = config.castor_prefix + dtTtrigValid.crab_opts['USERDIRCAF']
     haddInCastor(castor_dir,result_file,'residuals')
 
-def runTtrigResidualCorr(run,result_dir):
-    dtTtrigResidualCorr = DTTTrigResidualCorr(run,result_dir)  
+def runTtrigResidualCorr(run,config):
+    dtTtrigResidualCorr = DTTTrigResidualCorr(run,config.result_dir)  
     dtTtrigResidualCorr.run()
 
     print "Finished processing:"
     for pset in dtTtrigResidualCorr.configs: print "--->",pset
 
-def runTtrigValidResidCorr(run,runselection,trial,input_file,result_dir):
+def runTtrigValidResidCorr(run,runselection,trial,input_file,config):
     #ttrig_ResidCorr_db = os.path.abspath(result_dir + '/' + 'ttrig_ResidCorr_' + run + '.db')
 
     crab_opts = {'RUNSELECTION':runselection,
                  'USERDIRCAF':'TTRIGCalibration/Validation/ResidCorr/Run' + str(run) + '/v' + str(trial),
                  'INPUTFILE':input_file}
 
-    crab_opts.update(def_crab_opts)
+    crab_opts.update(config.crab_opts)
 
     pset_opts = {'INPUTFILE':input_file.split('/')[-1]}
-    pset_opts.update(def_pset_opts)
+    pset_opts.update(config.pset_opts)
 
     dtTtrigValid_ResidCorr = DTTTrigValid(run,crab_opts,pset_opts) 
     project_valid_ResidCorr = dtTtrigValid_ResidCorr.run()
@@ -101,14 +103,14 @@ def runTtrigValidResidCorr(run,runselection,trial,input_file,result_dir):
     crabValidResidCorr.start()
     crabValidResidCorr.join()
 
-    result_file = result_dir + '/DTkFactValidation_ResidCorr_%s.root'%run
-    castor_dir = castor_prefix + dtTtrigValid_ResidCorr.crab_opts['USERDIRCAF']
+    result_file = config.result_dir + '/DTkFactValidation_ResidCorr_%s.root'%run
+    castor_dir = config.castor_prefix + dtTtrigValid_ResidCorr.crab_opts['USERDIRCAF']
     haddInCastor(castor_dir,result_file,'residuals')
 
-def runTtrigValidSummary(run,input_file,result_dir):
+def runTtrigValidSummary(run,input_file,config):
     #input_file = os.path.abspath(result_file)
 
-    dtTtrigValidSummary = DTValidSummary(run,input_file,result_dir)
+    dtTtrigValidSummary = DTValidSummary(run,input_file,config.result_dir)
     dtTtrigValidSummary.run()
 
     print "Finished processing:"
@@ -120,6 +122,7 @@ if __name__ == '__main__':
     run = None
     runselection = None
     trial = None
+    config_mod = None
     #castor_prefix = 'rfio:/castor/cern.ch/cms/store/caf/user/antoniov/'
     for opt in sys.argv:
         if opt[:4] == 'run=':
@@ -128,23 +131,34 @@ if __name__ == '__main__':
             runselection = opt[13:] 
         if opt[:6] == 'trial=':
             trial = opt[6:]
-        if opt[:7] == 'castor=':
-            castor_prefix = opt[7:]
+        if opt[:7] == 'config=':
+            config_mod = opt[7:]
+        #if opt[:7] == 'castor=':
+            #castor_prefix = opt[7:]
 
-    if not run: raise ValueError,'Need to set run number'
-    if not trial: raise ValueError,'Need to set trial number'
+    if not run: raise RuntimeError,'Need to set run number'
+    if not trial: raise RuntimeError,'Need to set trial number'
+    if not config_mod: raise RuntimeError,'Need to set name of python file with config. options'
     #if not castor_prefix: raise ValueError,'Need to set castor dir'
 
     if not runselection: runselection = run
 
+    config = __import__(config_mod)
+    configFields = ['castor_prefix','crab_opts','pset_opts']
+    for item in configFields:
+        if not config.hasattr(item):
+            raise RuntimeError,'Need to set "%s" in %s.py' % (item,config_mod)
+     
     result_dir = 'Run%s'%run
     result_dir += '/Ttrig/Results'
     if not os.path.exists(result_dir): os.makedirs(result_dir)
+    config.result_dir = result_dir
 
     log_dir = 'Run%s'%run
     log_dir += '/Ttrig/Log'
     if not os.path.exists(log_dir): os.makedirs(log_dir)
- 
+    config.log_dir = log_dir 
+
     #logFileName = os.path.abspath('%s/Run_%s_v%s.log' % (log_dir,run,trial))
     #logOut = open(logFileName,'w',1)
 
@@ -158,37 +172,37 @@ if __name__ == '__main__':
 
     timeBoxes = os.path.abspath(result_dir + '/' + 'DTTimeBoxes_' + run + '.root')
 
-    if not os.path.exists(timeBoxes): runTtrigProd(run,runselection,trial,result_dir)
+    if not os.path.exists(timeBoxes): runTtrigProd(run,runselection,trial,config)
    
     if not os.path.exists(timeBoxes): raise RuntimeError,'Could not produce %s'%timeBoxes
 
     ttrig_second_db = os.path.abspath(result_dir + '/' + 'ttrig_second_' + run + '.db')
 
-    if not os.path.exists(ttrig_second_db): runTtrigCorrFirst(run,result_dir)
+    if not os.path.exists(ttrig_second_db): runTtrigCorrFirst(run,config)
 
     if not os.path.exists(ttrig_second_db): raise RuntimeError,'Could not produce %s'%ttrig_second_db
 
     residualsFirst = os.path.abspath(result_dir + '/' + 'DTkFactValidation_' + run + '.root')
 
-    if not os.path.exists(residualsFirst): runTtrigValid(run,runselection,trial,ttrig_second_db,result_dir)
+    if not os.path.exists(residualsFirst): runTtrigValid(run,runselection,trial,ttrig_second_db,config)
 
     if not os.path.exists(residualsFirst): raise RuntimeError,'Could not produce %s'%residualsFirst
 
     ttrig_ResidCorr_db = os.path.abspath(result_dir + '/' + 'ttrig_ResidCorr_' + run + '.db')
 
-    if not os.path.exists(ttrig_ResidCorr_db): runTtrigResidualCorr(run,result_dir)
+    if not os.path.exists(ttrig_ResidCorr_db): runTtrigResidualCorr(run,config)
 
     if not os.path.exists(ttrig_ResidCorr_db): raise RuntimeError,'Could not produce %s'%ttrig_ResidCorr_db      
 
     residualsResidCorr = os.path.abspath(result_dir + '/' + 'DTkFactValidation_ResidCorr_' + run + '.root')
     
-    if not os.path.exists(residualsResidCorr): runTtrigValidResidCorr(run,runselection,trial,ttrig_ResidCorr_db,result_dir)
+    if not os.path.exists(residualsResidCorr): runTtrigValidResidCorr(run,runselection,trial,ttrig_ResidCorr_db,config)
 
     if not os.path.exists(residualsResidCorr): raise RuntimeError,'Could not produce %s'%residualsResidCorr
 
     summaryResiduals = os.path.abspath(result_dir + '/' + 'SummaryResiduals_' + run + '.root')
 
-    if not os.path.exists(summaryResiduals): runTtrigValidSummary(run,residualsResidCorr,result_dir)
+    if not os.path.exists(summaryResiduals): runTtrigValidSummary(run,residualsResidCorr,config)
 
     if not os.path.exists(summaryResiduals): raise RuntimeError,'Could not produce %s'%summaryResiduals
 
---
+++ CalibMuon/DTCalibration/Workflow/options.py
@@ -0,0 +1,6 @@
+castor_prefix = '/castor/cern.ch/cms/store/caf/user/antoniov/'
+    
+crab_opts = {'DATASETPATH':'/StreamExpress/CRAFT09-MuAlCalIsolatedMu-v1/ALCARECO',
+             'EMAIL':'vilela@to.infn.it'}
+    
+pset_opts = {'GLOBALTAG':'GR09_P_V1::All'}
