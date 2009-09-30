de3df12ad9bd419583c8427445a8ec3cc907c9d3
antoniov <>
2009-09-30 18:55:55 +0000

    update

--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/jetResponse.C
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/jetResponse.C
@@ -62,26 +62,26 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
          // Ratio of RecoJetpT and GenJetpT
          sprintf(hname,"response_%i_%i",i_eta,i_pt);
          sprintf(title,"p_{T}(Reco)/p_{T}(Gen) #in %f < #||{#eta} < %f and #in p_{T} #left[%f , %f #right]GeV",etaBoundaries[i_eta],etaBoundaries[i_eta+1],ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
-         std::cout << hname << std::endl;
+         //std::cout << hname << std::endl;
          histos[index][0] = new TH1F(hname,title,100,0.,2.);
 	
          // RecoJetpT - GenJetpT
          sprintf(hname,"pTdistribution_%i_%i",i_eta,i_pt);
          sprintf(title,"p_{T}(Reco) - p_{T}(Gen) #in %f < #||{#eta} < %f and #in p_{T} #left[%f , %f #right]GeV",etaBoundaries[i_eta],etaBoundaries[i_eta+1],ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
-         std::cout << hname << std::endl;
+         //std::cout << hname << std::endl;
          histos[index][1] = new TH1F(hname,title,100,-100.,100.);
 
+         // RecoJetEta - GenJetEta
+         sprintf(hname,"etadistribution_%i_%i",i_eta,i_pt);
+         sprintf(title,"#||{#eta_{Reco}}-#||{#eta_{Gen}} #in %f < #||{#eta} < %f and #in p_{T} #left[%f , %f #right]GeV",etaBoundaries[i_eta],etaBoundaries[i_eta+1],ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
+         //std::cout << hname << std::endl;
+         histos[index][2] = new TH1F(hname,title,100,-0.5,0.5);
+ 
        	 // RecoJetPhi - GenJetPhi
          sprintf(hname,"phidistribution_%i_%i",i_eta,i_pt);
          sprintf(title,"#phi_{Reco}-#phi_{Gen} #in %f < #||{#eta} < %f and #in p_{T} #left[%f , %f #right]GeV",etaBoundaries[i_eta],etaBoundaries[i_eta+1],ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
-         std::cout << hname << std::endl;
-         histos[index][2] = new TH1F(hname,title,100,-1.0,1.0);
-
-       	 // RecoJetEta - GenJetEta
-         sprintf(hname,"etadistribution_%i_%i",i_eta,i_pt);
-         sprintf(title,"#||{#eta_{Reco}}-#||{#eta_{Gen}} #in %f < #||{#eta} < %f and #in p_{T} #left[%f , %f #right]GeV",etaBoundaries[i_eta],etaBoundaries[i_eta+1],ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
-         std::cout << hname << std::endl;
-         histos[index][3] = new TH1F(hname,title,100,-0.5,0.5);
+         //std::cout << hname << std::endl;
+         histos[index][3] = new TH1F(hname,title,100,-1.0,1.0);
 
          /*// Ratio RecoJetpT/GenJetpT vs pt
          sprintf(hname,"resp_%i_%i",i_eta,i_pt);
@@ -91,17 +91,33 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
 
    std::map<int, std::vector<TH1F*> > histosVsPt;
    for(size_t i_eta = 0; i_eta < (etaBoundaries.size() - 1); ++i_eta){
-      histosVsPt[i_eta] = std::vector<TH1F*>(2);
+      histosVsPt[i_eta] = std::vector<TH1F*>(6);
 
       char hname[30];
       char title[100];
-      sprintf(hname,"meanVsPt_%i",i_eta);
+      sprintf(hname,"meanRespVsPt_%i",i_eta);
       sprintf(title,"<response> vs p^{GenJet}_{T} - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
       histosVsPt[i_eta][0] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
 
-      sprintf(hname,"sigmaVsPt_%i",i_eta);
+      sprintf(hname,"sigmaRespVsPt_%i",i_eta);
       sprintf(title,"Jet p^{GenJet}_{T} resolution - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
       histosVsPt[i_eta][1] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
+
+      sprintf(hname,"meanEtaVsPt_%i",i_eta);
+      sprintf(title,"<#eta> vs p^{GenJet}_{T} - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
+      histosVsPt[i_eta][2] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
+
+      sprintf(hname,"sigmaEtaVsPt_%i",i_eta);
+      sprintf(title,"#sigma(#eta) vs p^{GenJet}_{T} - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
+      histosVsPt[i_eta][3] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
+
+      sprintf(hname,"meanPhiVsPt_%i",i_eta);
+      sprintf(title,"<#phi> vs p^{GenJet}_{T} - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
+      histosVsPt[i_eta][4] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
+
+      sprintf(hname,"sigmaPhiVsPt_%i",i_eta);
+      sprintf(title,"#sigma(#phi) vs p^{GenJet}_{T} - %f < #||{#eta} < %f",etaBoundaries[i_eta],etaBoundaries[i_eta+1]);
+      histosVsPt[i_eta][5] = new TH1F(hname,title,(ptBoundaries.size() - 1),aux_PtBoundaries);
    }
 
    std::map<int, std::vector<TH1F*> > histosVsEta;
@@ -110,11 +126,11 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
 
       char hname[30];
       char title[100];
-      sprintf(hname,"meanVsEta_%i",i_pt);
+      sprintf(hname,"meanRespVsEta_%i",i_pt);
       sprintf(title,"<response> vs #eta^{RecoJet} #in p_{T} #left[%f , %f #right]GeV",ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
       histosVsEta[i_pt][0] = new TH1F(hname,title,(etaBoundaries.size() - 1),aux_EtaBoundaries);
 
-      sprintf(hname,"sigmaVsEta_%i",i_pt);
+      sprintf(hname,"sigmaRespVsEta_%i",i_pt);
       sprintf(title,"Jet p^{GenJet}_{T} resolution #in p_{T} #left[%f , %f #right]GeV",ptBoundaries[i_pt],ptBoundaries[i_pt+1]);
       histosVsEta[i_pt][1] = new TH1F(hname,title,(etaBoundaries.size() - 1),aux_EtaBoundaries);
    }
@@ -149,8 +165,8 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
          std::vector<TH1F*>& myhistos = it_histos->second;
          myhistos[0]->Fill(response);
          myhistos[1]->Fill(ptDiff);
-         myhistos[2]->Fill(phiDiff);
-         myhistos[3]->Fill(etaDiff);
+         myhistos[2]->Fill(etaDiff);
+         myhistos[3]->Fill(phiDiff);
       }
    }
 
@@ -240,32 +256,71 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
          h_ptDiff->GetXaxis()->SetLabelSize(0.04);
          h_ptDiff->Draw();
 
-         // Draw phiDiff histo
-         sprintf(cname,"c_phidistribution_%i_%i",i_eta,i_pt);
+         // Draw etaDiff histo
+         sprintf(cname,"c_etadistribution_%i_%i",i_eta,i_pt);
          canvases[index][2] = new TCanvas(cname,cname,900,700);
          canvases[index][2]->cd();
 
-         TH1F* h_phiDiff = myhistos[2];
+         TH1F* h_etaDiff = myhistos[2];
+         h_etaDiff->GetXaxis()->SetTitle("#||{#eta^{RecoJet}} - #||{#eta^{GenJet}}");
+         h_etaDiff->GetYaxis()->SetTitle("Events");
+         h_etaDiff->GetYaxis()->SetLabelSize(0.04);
+         h_etaDiff->GetXaxis()->SetLabelSize(0.04);
+         h_etaDiff->Draw();
+ 
+         // Fit eta histo
+         fitMin = h_etaDiff->GetMean() - gausswidth*h_etaDiff->GetRMS();
+         fitMax = h_etaDiff->GetMean() + gausswidth*h_etaDiff->GetRMS();
+         h_etaDiff->Fit("gaus","RIE","",fitMin,fitMax);
+         const TF1* fitEta = static_cast<TF1*>(h_etaDiff->GetFunction("gaus"));
+
+         double meanEta = fitEta->GetParameter(1);
+         double meanEtaErr = fitEta->GetParError(1);
+         double sigmaEta = fitEta->GetParameter(2);
+         double sigmaEtaErr = fitEta->GetParError(2);
+
+         // Fill histos vs pt
+         TH1F* h_meanEtaVsPt = myhistosVsPt[2];
+         TH1F* h_sigmaEtaVsPt = myhistosVsPt[3];
+         h_sigmaEtaVsPt->SetBinContent(h_sigmaEtaVsPt->GetXaxis()->FindBin(x_binPt),sigmaEta);
+         h_sigmaEtaVsPt->SetBinError(h_sigmaEtaVsPt->GetXaxis()->FindBin(x_binPt),sigmaEtaErr);
+         h_meanEtaVsPt->SetBinContent(h_meanEtaVsPt->GetXaxis()->FindBin(x_binPt),meanEta);
+         h_meanEtaVsPt->SetBinError(h_meanEtaVsPt->GetXaxis()->FindBin(x_binPt),meanEtaErr);
+
+         // Draw phiDiff histo
+         sprintf(cname,"c_phidistribution_%i_%i",i_eta,i_pt);
+         canvases[index][3] = new TCanvas(cname,cname,900,700);
+         canvases[index][3]->cd();
+
+         TH1F* h_phiDiff = myhistos[3];
          h_phiDiff->GetXaxis()->SetTitle("#phi^{RecoJet} - #phi^{GenJet}");
          h_phiDiff->GetYaxis()->SetTitle("Events");
          h_phiDiff->GetYaxis()->SetLabelSize(0.04);
          h_phiDiff->GetXaxis()->SetLabelSize(0.04);
          h_phiDiff->Draw();
 
-         // Draw etaDiff histo
-         sprintf(cname,"c_etadistribution_%i_%i",i_eta,i_pt);
-         canvases[index][3] = new TCanvas(cname,cname,900,700);
-         canvases[index][3]->cd();
+         // Fit eta histo
+         fitMin = h_phiDiff->GetMean() - gausswidth*h_phiDiff->GetRMS();
+         fitMax = h_phiDiff->GetMean() + gausswidth*h_phiDiff->GetRMS();
+         h_phiDiff->Fit("gaus","RIE","",fitMin,fitMax);
+         const TF1* fitPhi = static_cast<TF1*>(h_phiDiff->GetFunction("gaus"));
 
-         TH1F* h_etaDiff = myhistos[3];
-         h_etaDiff->GetXaxis()->SetTitle("#||{#eta^{RecoJet}} - #||{#eta^{GenJet}}");
-         h_etaDiff->GetYaxis()->SetTitle("Events");
-         h_etaDiff->GetYaxis()->SetLabelSize(0.04);
-         h_etaDiff->GetXaxis()->SetLabelSize(0.04);
-         h_etaDiff->Draw();
+         double meanPhi = fitPhi->GetParameter(1);
+         double meanPhiErr = fitPhi->GetParError(1);
+         double sigmaPhi = fitPhi->GetParameter(2);
+         double sigmaPhiErr = fitPhi->GetParError(2);
+
+         // Fill histos vs pt
+         TH1F* h_meanPhiVsPt = myhistosVsPt[4];
+         TH1F* h_sigmaPhiVsPt = myhistosVsPt[5];
+         h_sigmaPhiVsPt->SetBinContent(h_sigmaPhiVsPt->GetXaxis()->FindBin(x_binPt),sigmaPhi);
+         h_sigmaPhiVsPt->SetBinError(h_sigmaPhiVsPt->GetXaxis()->FindBin(x_binPt),sigmaPhiErr);
+         h_meanPhiVsPt->SetBinContent(h_meanPhiVsPt->GetXaxis()->FindBin(x_binPt),meanPhi);
+         h_meanPhiVsPt->SetBinError(h_meanPhiVsPt->GetXaxis()->FindBin(x_binPt),meanPhiErr);
       }
    }
    
+   // Plot histos vs pt 
    std::map<int, std::vector<TCanvas*> > canvasesVsPt;
    std::map<int, TF1*> fitResolutionVsPt; 
    for(size_t i_eta = 0; i_eta < (etaBoundaries.size() - 1); ++i_eta){
@@ -276,26 +331,26 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
       }
       std::vector<TH1F*>& myhistosVsPt = it_histosVsPt->second;
 
-      canvasesVsPt[i_eta] = std::vector<TCanvas*>(2);
+      canvasesVsPt[i_eta] = std::vector<TCanvas*>(6);
  
       char cname[30];
-      sprintf(cname,"c_meanVsPt_%i",i_eta);
+      sprintf(cname,"c_meanRespVsPt_%i",i_eta);
       canvasesVsPt[i_eta][0] = new TCanvas(cname,cname,900,700);
       canvasesVsPt[i_eta][0]->cd(); 
 
       TH1F* h_mean = myhistosVsPt[0];
-      h_mean->GetXaxis()->SetRangeUser(20.,100.);
+      h_mean->GetXaxis()->SetRangeUser(10.,200.);
       h_mean->GetYaxis()->SetRangeUser(0.,1.5);
       h_mean->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
       h_mean->GetYaxis()->SetTitle("< #frac{p^{RecoJet}_{T}}{p^{GenJet}_{T}} >");
       h_mean->Draw();
 
-      sprintf(cname,"c_sigmaVsPt_%i",i_eta);
+      sprintf(cname,"c_sigmaRespVsPt_%i",i_eta);
       canvasesVsPt[i_eta][1] = new TCanvas(cname,cname,900,700);
       canvasesVsPt[i_eta][1]->cd();
 
       TH1F* h_sigma = myhistosVsPt[1];
-      h_sigma->GetXaxis()->SetRangeUser(20.,100.);
+      h_sigma->GetXaxis()->SetRangeUser(10.,200.);
       h_sigma->GetYaxis()->SetRangeUser(0.,0.35);
       h_sigma->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
       h_sigma->GetYaxis()->SetTitle("#sigma(#frac{p^{RecoJet}_{T}}{p^{GenJet}_{T}})");
@@ -318,8 +373,53 @@ void jetResponse(std::string const& fileName, std::string const& treeName = "jet
       h_sigma->SetLineColor(2);
       h_sigma->GetYaxis()->SetLabelSize(0.03);  
       h_sigma->Draw();
+
+      sprintf(cname,"c_meanEtaVsPt_%i",i_eta);
+      canvasesVsPt[i_eta][2] = new TCanvas(cname,cname,900,700);
+      canvasesVsPt[i_eta][2]->cd();
+
+      TH1F* h_meanEta = myhistosVsPt[2];
+      h_meanEta->GetXaxis()->SetRangeUser(10.,200.);
+      h_meanEta->GetYaxis()->SetRangeUser(-0.3,0.3);
+      h_meanEta->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
+      h_meanEta->GetYaxis()->SetTitle("<#eta>");
+      h_meanEta->Draw();
+
+      sprintf(cname,"c_sigmaEtaVsPt_%i",i_eta);
+      canvasesVsPt[i_eta][3] = new TCanvas(cname,cname,900,700);
+      canvasesVsPt[i_eta][3]->cd();
+
+      TH1F* h_sigmaEta = myhistosVsPt[3];
+      h_sigmaEta->GetXaxis()->SetRangeUser(10.,200.);
+      h_sigmaEta->GetYaxis()->SetRangeUser(0.,0.12);
+      h_sigmaEta->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
+      h_sigmaEta->GetYaxis()->SetTitle("#sigma(#eta)");
+      h_sigmaEta->Draw();
+
+      sprintf(cname,"c_meanPhiVsPt_%i",i_eta);
+      canvasesVsPt[i_eta][4] = new TCanvas(cname,cname,900,700);
+      canvasesVsPt[i_eta][4]->cd();
+
+      TH1F* h_meanPhi = myhistosVsPt[4];
+      h_meanPhi->GetXaxis()->SetRangeUser(10.,200.);
+      h_meanPhi->GetYaxis()->SetRangeUser(-0.3,0.3);
+      h_meanPhi->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
+      h_meanPhi->GetYaxis()->SetTitle("<#phi>");
+      h_meanPhi->Draw();
+
+      sprintf(cname,"c_sigmaPhiVsPt_%i",i_eta);
+      canvasesVsPt[i_eta][5] = new TCanvas(cname,cname,900,700);
+      canvasesVsPt[i_eta][5]->cd();
+
+      TH1F* h_sigmaPhi = myhistosVsPt[5];
+      h_sigmaPhi->GetXaxis()->SetRangeUser(10.,200.);
+      h_sigmaPhi->GetYaxis()->SetRangeUser(0.,0.12);
+      h_sigmaPhi->GetXaxis()->SetTitle("p^{GenJet}_{T}#left[GeV#right]");
+      h_sigmaPhi->GetYaxis()->SetTitle("#sigma(#phi)");
+      h_sigmaPhi->Draw();
    }
 
+   // Plot histos vs eta
    std::map<int, std::vector<TCanvas*> > canvasesVsEta;
    //std::map<int, TF1*> fitResolutionVsEta;
    for(size_t i_pt = 0; i_pt < (ptBoundaries.size() - 1); ++i_pt){
