efd5fa09de20539dff9868cd06cea193f8fdd44b
antoniov <>
2009-10-09 18:51:10 +0000

    update

--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/jetResponseAnalysis_cfg.py
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/test/jetResponseAnalysis_cfg.py
@@ -38,26 +38,57 @@ makeAnalysis(process,'',attributes,filters)
 """
 
 from ExclusiveDijetsAnalysis.ExclusiveDijetsAnalysis.leadingJets_cfi import leadingJets
-process.leadingGenJetsSC5 = leadingJets.clone(src = cms.InputTag("sisCone5GenJets"))
-process.leadingGenJetsSC7 = leadingJets.clone(src = cms.InputTag("sisCone7GenJets"))
 
 matchGenToRecoJet = cms.EDProducer("TrivialDeltaRViewMatcher",
     src = cms.InputTag(""),
     matched = cms.InputTag(""),
     distMin = cms.double(0.25)
 )
-process.matchGenToRecoJetSC5Calo = matchGenToRecoJet.clone(src="leadingGenJetsSC5",matched="L2L3CorJetSC5Calo")
-process.matchGenToRecoJetSC7Calo = matchGenToRecoJet.clone(src="leadingGenJetsSC7",matched="L2L3CorJetSC7Calo")
-process.matchGenToRecoJetSC5PF = matchGenToRecoJet.clone(src="leadingGenJetsSC5",matched="L2L3CorJetSC5PF")
-process.matchGenToRecoJetSC7PF = matchGenToRecoJet.clone(src="leadingGenJetsSC7",matched="L2L3CorJetSC7PF")
 
 jetResponseAnalysis = cms.EDAnalyzer("SimpleJetResponseAnalyzer",
    GenToRecoJetMatchTag = cms.InputTag("")
 )
-process.jetResponseAnalysisSC5Calo = jetResponseAnalysis.clone(GenToRecoJetMatchTag="matchGenToRecoJetSC5Calo")
-process.jetResponseAnalysisSC7Calo = jetResponseAnalysis.clone(GenToRecoJetMatchTag="matchGenToRecoJetSC7Calo")
-process.jetResponseAnalysisSC5PF = jetResponseAnalysis.clone(GenToRecoJetMatchTag="matchGenToRecoJetSC5PF")
-process.jetResponseAnalysisSC7PF = jetResponseAnalysis.clone(GenToRecoJetMatchTag="matchGenToRecoJetSC7PF")
+
+from DiffractiveForwardAnalysis.SingleDiffractiveWAnalysis.analysisTools import *
+
+#jetAlgos = ['KT4','KT6','SC5','SC7']
+#jetTypes = ['Calo','PF']
+
+jetAlgos = {'IC5':('Calo','PF'),
+            'KT4':('Calo','PF'),
+            'KT6':('Calo','PF'),
+            'SC5':('Calo','PF'),
+            'SC7':('Calo','PF')}
+
+collPrefix = 'L2L3CorJet'
+for algo in jetAlgos:
+    algoFullName = ((algo.replace('KT','kt')).replace('SC','sisCone')).replace('IC','iterativeCone')
+    genCollLabel = algoFullName + 'GenJets'
+    leadingJetsLabel = 'leadingGenJets' + algo
+    if not hasattr(process,leadingJetsLabel):
+        setattr(process,leadingJetsLabel,leadingJets.clone(src=genCollLabel))
+
+    for type in jetAlgos[algo]:
+        corrCollLabel = collPrefix + algo + type
+        matchGenToRecoJetlabel = 'matchGenToRecoJet' + algo + type
+        if not hasattr(process,matchGenToRecoJetlabel):
+            setattr(process,matchGenToRecoJetlabel,matchGenToRecoJet.clone(src=genCollLabel,matched=corrCollLabel)) 
+
+        jetResponseAnalysisLabel = 'jetResponseAnalysis' + algo + type
+        if not hasattr(process,jetResponseAnalysisLabel):
+            setattr(process,jetResponseAnalysisLabel,jetResponseAnalysis.clone(GenToRecoJetMatchTag=matchGenToRecoJetlabel))
+
+        sequenceLabel = "analysis_" + algo + type
+        if not hasattr(process,sequenceLabel):
+            setattr(process,sequenceLabel,cms.Sequence((getattr(process,leadingJetsLabel)+
+                                                       getattr(process,corrCollLabel))*
+                                                       getattr(process,matchGenToRecoJetlabel)*
+                                                       getattr(process,jetResponseAnalysisLabel)))
+
+        print "Adding",sequenceLabel
+        pathLabel = sequenceLabel + "_step"  
+        if not hasattr(process,pathLabel):
+            setattr(process,pathLabel,cms.Path(getattr(process,sequenceLabel)))
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("jetResponseAnalysis_TTree.root")
@@ -69,11 +100,4 @@ process.output.fileName = '/data1/antoniov/jetResponse_test.root'
 #process.output.SelectEvents.SelectEvents = cms.vstring('selection_step')
 
 #process.reco_step = cms.Path(process.L2L3CorJetSC7PF)
-process.reco_step = cms.Path(process.L2L3CorJetSC5Calo+process.L2L3CorJetSC7Calo+process.L2L3CorJetSC5PF+process.L2L3CorJetSC7PF)
-process.analysis_step = cms.Path((process.leadingGenJetsSC5+process.leadingGenJetsSC7)*
-                                 (process.matchGenToRecoJetSC5Calo+process.matchGenToRecoJetSC7Calo+
-                                 process.matchGenToRecoJetSC5PF+process.matchGenToRecoJetSC7PF)*
-                                 (process.jetResponseAnalysisSC5Calo+process.jetResponseAnalysisSC7Calo+
-                                 process.jetResponseAnalysisSC5PF+process.jetResponseAnalysisSC7PF)) 
-
 #process.out_step = cms.EndPath(process.output)
