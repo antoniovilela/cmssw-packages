d3fbbeb8ae932f97424f68d48d3ae2ecd1169695
antoniov <>
2009-10-13 10:23:13 +0000

    update

--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/interface/Histos.h
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/interface/Histos.h
@@ -11,132 +11,77 @@
 
 namespace exclusiveDijetsAnalysis {
   // Histograms used for analysis
-  struct Histos {
-     TH1F* h_nPileUpBx0_;
-
-     TH1F* h_nVertex_;
-
-     TH1F* h_leadingJetPt_;
-     TH1F* h_leadingJetEta_;
-     TH1F* h_leadingJetPhi_;
-     TH1F* h_leadingJetBDiscriminator_;
-   
-     TH1F* h_secondJetPt_;
-     TH1F* h_secondJetEta_;
-     TH1F* h_secondJetPhi_;
-     TH1F* h_secondJetBDiscriminator_;
-
-     TH1F* h_thirdJetPt_;
-     TH1F* h_thirdJetEta_;
-
-     TH1F* h_jetsAveEta_;
-     TH1F* h_jetsDeltaEta_;
-     TH1F* h_jetsDeltaPhi_;
-     TH1F* h_jetsDeltaPt_;
-
-     TH1F* h_trackMultiplicity_;
-     TH1F* h_multiplicityHFPlus_;
-     TH1F* h_multiplicityHFMinus_;
-
-     TH2F* h_iEtaVsHFCountPlus_;
-     TH2F* h_iEtaVsHFCountMinus_;
-  
-     TH1F* h_xiGenPlus_;
-     TH1F* h_xiGenMinus_;
-     TH1F* h_xiTowerPlus_;
-     TH1F* h_xiTowerMinus_;
-     TH1F* h_xiPlusFromJets_;
-     TH1F* h_xiMinusFromJets_;
-     TH1F* h_xiPlusFromPFCands_;
-     TH1F* h_xiMinusFromPFCands_;
-     TH1F* h_ResXiTowerPlus_;
-     TH1F* h_ResXiTowerMinus_;
-     TH1F* h_ResXiPlusFromJets_;
-     TH1F* h_ResXiMinusFromJets_;
-     TH1F* h_ResXiPlusFromPFCands_;
-     TH1F* h_ResXiMinusFromPFCands_;
-     TH2F* h_xiPlusVsxiGenPlus_;
-     TH2F* h_xiMinusVsxiGenMinus_;
-      
-     TH1F* h_massDijets_;
-     TH1F* h_massDijetsGen_; 
-     TH1F* h_missingMassFromXi_;
-     TH1F* h_MxFromJets_;
-     TH1F* h_RjjFromJets_;
-     TH1F* h_RjjFromPFCands_;
-     TH1F* h_ResMassDijets_;
-
-     TH1F* h_EnergyVsEta_;
-
-     TH2F* h_RjjFromJetsVsBDicriminator_;
-     TH2F* h_RjjFromPFCandsVsBDicriminator_;
-  };
+  typedef std::map<std::string,TH1F*> HistoMapTH1F;
+  typedef std::map<std::string,TH2F*> HistoMapTH2F;
 
   // Book histos
-  // FIXME: make this a template function allowing for different "adaptors"
-  //void bookHistos(Histos& histos, edm::Service<TFileService>& adaptor){
+  // FIXME: configure which histos to add
   template <class Adaptor>
-  void bookHistos(Histos& histos, Adaptor& adaptor){
-     histos.h_nPileUpBx0_ = adaptor.template make<TH1F>("nPileUpBx0","Nr. of pile-up events in Bx0",10,0,10);
+  void bookHistos(HistoMapTH1F& histos, const Adaptor& adaptor){
+     //pMakeTH1F = &Adaptor::template make<TH1F>;
 
-     histos.h_nVertex_ = adaptor.template make<TH1F>("nVertex","Nr. of offline primary vertexes",10,0,10);
+     histos["nPileUpBx0"] = adaptor.template make<TH1F>("nPileUpBx0","Nr. of pile-up events in Bx0",10,0,10);
+     histos["nVertex"] = adaptor.template make<TH1F>("nVertex","Nr. of offline primary vertexes",10,0,10);
 
-     histos.h_leadingJetPt_ = adaptor.template make<TH1F>("leadingJetPt","leadingJetPt",100,0.,100.);
-     histos.h_leadingJetEta_ = adaptor.template make<TH1F>("leadingJetEta","leadingJetEta",100,-5.,5.);
-     histos.h_leadingJetPhi_ = adaptor.template make<TH1F>("leadingJetPhi","leadingJetPhi",100,-1.1*M_PI,1.1*M_PI);
-     histos.h_leadingJetBDiscriminator_ = adaptor.template make<TH1F>("leadingJetBDiscriminator","leadingJetBDiscriminator",100,-10.,30.);
+     histos["leadingJetPt"] = adaptor.template make<TH1F>("leadingJetPt","leadingJetPt",100,0.,100.);
+     histos["leadingJetEta"] = adaptor.template make<TH1F>("leadingJetEta","leadingJetEta",100,-5.,5.);
+     histos["leadingJetPhi"] = adaptor.template make<TH1F>("leadingJetPhi","leadingJetPhi",100,-1.1*M_PI,1.1*M_PI);
+     histos["leadingJetBDiscriminator"] = adaptor.template make<TH1F>("leadingJetBDiscriminator","leadingJetBDiscriminator",100,-10.,30.);
  
-     histos.h_secondJetPt_ = adaptor.template make<TH1F>("secondJetPt","secondJetPt",100,0.,100.);
-     histos.h_secondJetEta_ = adaptor.template make<TH1F>("secondJetEta","secondJetEta",100,-5.,5.);
-     histos.h_secondJetPhi_ = adaptor.template make<TH1F>("secondJetPhi","secondJetPhi",100,-1.1*M_PI,1.1*M_PI);
-     histos.h_secondJetBDiscriminator_ = adaptor.template make<TH1F>("secondJetBDiscriminator","secondJetBDiscriminator",100,-10.,30.);
+     histos["secondJetPt"] = adaptor.template make<TH1F>("secondJetPt","secondJetPt",100,0.,100.);
+     histos["secondJetEta"] = adaptor.template make<TH1F>("secondJetEta","secondJetEta",100,-5.,5.);
+     histos["secondJetPhi"] = adaptor.template make<TH1F>("secondJetPhi","secondJetPhi",100,-1.1*M_PI,1.1*M_PI);
+     histos["secondJetBDiscriminator"] = adaptor.template make<TH1F>("secondJetBDiscriminator","secondJetBDiscriminator",100,-10.,30.);
 
-     histos.h_thirdJetPt_ = adaptor.template make<TH1F>("thirdJetPt","thirdJetPt",100,0.,80.);
-     histos.h_thirdJetEta_ = adaptor.template make<TH1F>("thirdJetEta","thirdJetEta",100,-5.,5.);
+     histos["thirdJetPt"] = adaptor.template make<TH1F>("thirdJetPt","thirdJetPt",100,0.,80.);
+     histos["thirdJetEta"] = adaptor.template make<TH1F>("thirdJetEta","thirdJetEta",100,-5.,5.);
 
-     histos.h_jetsAveEta_ = adaptor.template make<TH1F>("jetsAveEta","jetsAveEta",100,-5.,5.);
-     histos.h_jetsDeltaEta_ = adaptor.template make<TH1F>("jetsDeltaEta","jetsDeltaEta",100,-5.,5.);
-     histos.h_jetsDeltaPhi_ = adaptor.template make<TH1F>("jetsDeltaPhi","jetsDeltaPhi",100,0.,1.1*M_PI);
-     histos.h_jetsDeltaPt_ = adaptor.template make<TH1F>("jetsDeltaPt","jetsDeltaPt",100,0.,10.);
+     histos["jetsAveEta"] = adaptor.template make<TH1F>("jetsAveEta","jetsAveEta",100,-5.,5.);
+     histos["jetsDeltaEta"] = adaptor.template make<TH1F>("jetsDeltaEta","jetsDeltaEta",100,-5.,5.);
+     histos["jetsDeltaPhi"] = adaptor.template make<TH1F>("jetsDeltaPhi","jetsDeltaPhi",100,0.,1.1*M_PI);
+     histos["jetsDeltaPt"] = adaptor.template make<TH1F>("jetsDeltaPt","jetsDeltaPt",100,0.,10.);
 
-     histos.h_trackMultiplicity_ = adaptor.template make<TH1F>("trackMultiplicity","trackMultiplicity",20,0,20);
-     histos.h_multiplicityHFPlus_ = adaptor.template make<TH1F>("multiplicityHFPlus","multiplicityHFPlus",20,0,20);
-     histos.h_multiplicityHFMinus_ = adaptor.template make<TH1F>("multiplicityHFMinus","multiplicityHFMinus",20,0,20);
+     histos["trackMultiplicity"] = adaptor.template make<TH1F>("trackMultiplicity","trackMultiplicity",20,0,20);
+     histos["multiplicityHFPlus"] = adaptor.template make<TH1F>("multiplicityHFPlus","multiplicityHFPlus",20,0,20);
+     histos["multiplicityHFMinus"] = adaptor.template make<TH1F>("multiplicityHFMinus","multiplicityHFMinus",20,0,20);
 
-     histos.h_iEtaVsHFCountPlus_ = adaptor.template make<TH2F>("iEtaVsHFCountPlus","iEtaVsHFCountPlus",11,30,41,20,0,20);
-     histos.h_iEtaVsHFCountMinus_ = adaptor.template make<TH2F>("iEtaVsHFCountMinus","iEtaVsHFCountMinus",11,30,41,20,0,20);
+     histos["xiGenPlus"] = adaptor.template make<TH1F>("xiGenPlus","xiGenPlus",200,0.,1.);
+     histos["xiGenMinus"] = adaptor.template make<TH1F>("xiGenMinus","xiGenMinus",200,0.,1.);
+     histos["xiTowerPlus"] = adaptor.template make<TH1F>("xiTowerPlus","xiTowerPlus",200,0.,1.);
+     histos["xiTowerMinus"] = adaptor.template make<TH1F>("xiTowerMinus","xiTowerMinus",200,0.,1.);
+     histos["xiPlusFromJets"] = adaptor.template make<TH1F>("xiPlusFromJets","xiPlusFromJets",200,0.,1.);
+     histos["xiMinusFromJets"] = adaptor.template make<TH1F>("xiMinusFromJets","xiMinusFromJets",200,0.,1.);
+     histos["xiPlusFromPFCands"] = adaptor.template make<TH1F>("xiPlusFromPFCands","xiPlusFromPFCands",200,0.,1.);
+     histos["xiMinusFromPFCands"] = adaptor.template make<TH1F>("xiMinusFromPFCands","xiMinusFromPFCands",200,0.,1.);
 
-     histos.h_xiGenPlus_ = adaptor.template make<TH1F>("xiGenPlus","xiGenPlus",200,0.,1.);
-     histos.h_xiGenMinus_ = adaptor.template make<TH1F>("xiGenMinus","xiGenMinus",200,0.,1.);
-     histos.h_xiTowerPlus_ = adaptor.template make<TH1F>("xiTowerPlus","xiTowerPlus",200,0.,1.);
-     histos.h_xiTowerMinus_ = adaptor.template make<TH1F>("xiTowerMinus","xiTowerMinus",200,0.,1.);
-     histos.h_xiPlusFromJets_ = adaptor.template make<TH1F>("xiPlusFromJets","xiPlusFromJets",200,0.,1.);
-     histos.h_xiMinusFromJets_ = adaptor.template make<TH1F>("xiMinusFromJets","xiMinusFromJets",200,0.,1.);
-     histos.h_xiPlusFromPFCands_ = adaptor.template make<TH1F>("xiPlusFromPFCands","xiPlusFromPFCands",200,0.,1.);
-     histos.h_xiMinusFromPFCands_ = adaptor.template make<TH1F>("xiMinusFromPFCands","xiMinusFromPFCands",200,0.,1.);
+     histos["ResXiTowerPlus"] = adaptor.template make<TH1F>("ResXiTowerPlus","ResXiTowerPlus",100,-1.,1.);
+     histos["ResXiTowerMinus"] = adaptor.template make<TH1F>("ResXiTowerMinus","ResXiTowerMinus",100,-1.,1.);
+     histos["ResXiPlusFromJets"] = adaptor.template make<TH1F>("ResXiPlusFromJets","ResXiPlusFromJets",100,-1.,1.);
+     histos["ResXiMinusFromJets"] = adaptor.template make<TH1F>("ResXiMinusFromJets","ResXiMinusFromJets",100,-1.,1.);
+     histos["ResXiPlusFromPFCands"] = adaptor.template make<TH1F>("ResXiPlusFromPFCands","ResXiPlusFromPFCands",100,-1.,1.);
+     histos["ResXiMinusFromPFCands"] = adaptor.template make<TH1F>("ResXiMinusFromPFCands","ResXiMinusFromPFCands",100,-1.,1.);
 
-     histos.h_xiPlusVsxiGenPlus_ = adaptor.template make<TH2F>("xiPlusVsxiGenPlus","xiPlusVsxiGenPlus",100,0.,1.,100,0.,1.);
-     histos.h_xiMinusVsxiGenMinus_ = adaptor.template make<TH2F>("xiMinusVsxiGenMinus","xiMinusVsxiGenMinus",100,0.,1.,100,0.,1.);
+     histos["massDijets"] = adaptor.template make<TH1F>("massDijets","massDijets",200,-10.,400.);
+     histos["massDijetsGen"] = adaptor.template make<TH1F>("massDijetsGen","massDijetsGen",200,-10.,400.);
+     histos["missingMassFromXi"] = adaptor.template make<TH1F>("missingMassFromXi","missingMassFromXi",200,-10.,400.);
+     histos["MxFromJets"] = adaptor.template make<TH1F>("MxFromJets","MxFromJets",200,-10.,400.);
+     histos["RjjFromJets"] = adaptor.template make<TH1F>("RjjFromJets","RjjFromJets",200,-0.1,1.5);
+     histos["RjjFromPFCands"] = adaptor.template make<TH1F>("RjjFromPFCands","RjjFromPFCands",200,-0.1,1.5);
+     histos["ResMassDijets"] = adaptor.template make<TH1F>("ResMassDijets","ResMassDijets",100,-1.,1.);
 
-     histos.h_ResXiTowerPlus_ = adaptor.template make<TH1F>("ResXiTowerPlus","ResXiTowerPlus",100,-1.,1.);
-     histos.h_ResXiTowerMinus_ = adaptor.template make<TH1F>("ResXiTowerMinus","ResXiTowerMinus",100,-1.,1.);
-     histos.h_ResXiPlusFromJets_ = adaptor.template make<TH1F>("ResXiPlusFromJets","ResXiPlusFromJets",100,-1.,1.);
-     histos.h_ResXiMinusFromJets_ = adaptor.template make<TH1F>("ResXiMinusFromJets","ResXiMinusFromJets",100,-1.,1.);
-     histos.h_ResXiPlusFromPFCands_ = adaptor.template make<TH1F>("ResXiPlusFromPFCands","ResXiPlusFromPFCands",100,-1.,1.);
-     histos.h_ResXiMinusFromPFCands_ = adaptor.template make<TH1F>("ResXiMinusFromPFCands","ResXiMinusFromPFCands",100,-1.,1.);
+     histos["EnergyVsEta"] = adaptor.template make<TH1F>("EnergyVsEta","EnergyVsEta",300,-15.,15.);
+  }
 
-     histos.h_massDijets_ = adaptor.template make<TH1F>("massDijets","massDijets",200,-10.,400.);
-     histos.h_massDijetsGen_ = adaptor.template make<TH1F>("massDijetsGen","massDijetsGen",200,-10.,400.);
-     histos.h_missingMassFromXi_ = adaptor.template make<TH1F>("missingMassFromXi","missingMassFromXi",200,-10.,400.);
-     histos.h_MxFromJets_ = adaptor.template make<TH1F>("MxFromJets","MxFromJets",200,-10.,400.);
-     histos.h_RjjFromJets_ = adaptor.template make<TH1F>("RjjFromJets","RjjFromJets",200,-0.1,1.5);
-     histos.h_RjjFromPFCands_ = adaptor.template make<TH1F>("RjjFromPFCands","RjjFromPFCands",200,-0.1,1.5);
-     histos.h_ResMassDijets_ = adaptor.template make<TH1F>("ResMassDijets","ResMassDijets",100,-1.,1.);
+  template <class Adaptor>
+  void bookHistos(HistoMapTH2F& histos, const Adaptor& adaptor){
+     histos["iEtaVsHFCountPlus"] = adaptor.template make<TH2F>("iEtaVsHFCountPlus","iEtaVsHFCountPlus",11,30,41,20,0,20);
+     histos["iEtaVsHFCountMinus"] = adaptor.template make<TH2F>("iEtaVsHFCountMinus","iEtaVsHFCountMinus",11,30,41,20,0,20);
 
-     histos.h_EnergyVsEta_ = adaptor.template make<TH1F>("EnergyVsEta","EnergyVsEta",300,-15.,15.);
+     histos["xiPlusVsxiGenPlus"] = adaptor.template make<TH2F>("xiPlusVsxiGenPlus","xiPlusVsxiGenPlus",100,0.,1.,100,0.,1.);
+     histos["xiMinusVsxiGenMinus"] = adaptor.template make<TH2F>("xiMinusVsxiGenMinus","xiMinusVsxiGenMinus",100,0.,1.,100,0.,1.);
 
-     histos.h_RjjFromJetsVsBDicriminator_ = adaptor.template make<TH2F>("RjjFromJetsVsBDicriminator","RjjFromJetsVsBDicriminator",200,-0.1,1.5,200,-10.,30.);
-     histos.h_RjjFromPFCandsVsBDicriminator_ = adaptor.template make<TH2F>("RjjFromPFCandsVsBDicriminator","RjjFromPFCandsVsBDicriminator",200,-0.1,1.5,200,-10.,30.);
+     histos["RjjFromJetsVsBDicriminator"] = adaptor.template make<TH2F>("RjjFromJetsVsBDicriminator","RjjFromJetsVsBDicriminator",200,-0.1,1.5,200,-10.,30.);
+     histos["RjjFromPFCandsVsBDicriminator"] = adaptor.template make<TH2F>("RjjFromPFCandsVsBDicriminator","RjjFromPFCandsVsBDicriminator",200,-0.1,1.5,200,-10.,30.);
   }
 
 } // namespace
--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/plugins/ExclusiveDijetsTTreeAnalyzer.cc
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/plugins/ExclusiveDijetsTTreeAnalyzer.cc
@@ -29,7 +29,8 @@ class ExclusiveDijetsTTreeAnalyzer: public edm::EDAnalyzer
 
     TTree* data_; // not owned
     exclusiveDijetsAnalysis::EventData eventData_;
-    exclusiveDijetsAnalysis::Histos histos_; // not owned
+    exclusiveDijetsAnalysis::HistoMapTH1F histosTH1F_; // histos owned by Service
+    exclusiveDijetsAnalysis::HistoMapTH2F histosTH2F_;
 };
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
@@ -84,7 +85,8 @@ void ExclusiveDijetsTTreeAnalyzer::beginJob(const edm::EventSetup& setup){
     addTTreeBranches(*data_,eventData_);
   }
 
-  bookHistos(histos_,*fs);
+  bookHistos(histosTH1F_,*fs);
+  bookHistos(histosTH2F_,*fs);
 }
 
 void ExclusiveDijetsTTreeAnalyzer::beginRun(const edm::Run& run, const edm::EventSetup& setup){
@@ -95,66 +97,69 @@ void ExclusiveDijetsTTreeAnalyzer::analyze(const edm::Event& event, const edm::E
 
   exclusiveDijetsAnalysisImpl_.fillEventData(eventData_,event,setup);
 
-  histos_.h_nPileUpBx0_->Fill(eventData_.nPileUpBx0_);
-  histos_.h_nVertex_->Fill(eventData_.nVertex_); 
+  histosTH1F_["nPileUpBx0"]->Fill(eventData_.nPileUpBx0_);
+  histosTH1F_["nVertex"]->Fill(eventData_.nVertex_); 
 
-  histos_.h_leadingJetPt_->Fill(eventData_.leadingJetPt_);
-  histos_.h_secondJetPt_->Fill(eventData_.secondJetPt_);
-  histos_.h_leadingJetEta_->Fill(eventData_.leadingJetEta_);
-  histos_.h_secondJetEta_->Fill(eventData_.secondJetEta_);
-  histos_.h_leadingJetPhi_->Fill(eventData_.leadingJetPhi_);
-  histos_.h_secondJetPhi_->Fill(eventData_.secondJetPhi_);  
-  histos_.h_thirdJetPt_->Fill(eventData_.thirdJetPt_);
-  histos_.h_thirdJetEta_->Fill(eventData_.thirdJetEta_);
-  histos_.h_jetsAveEta_->Fill(eventData_.jetsAveEta_);
-  histos_.h_jetsDeltaEta_->Fill(eventData_.jetsDeltaEta_);
-  histos_.h_jetsDeltaPhi_->Fill(eventData_.jetsDeltaPhi_);
-  histos_.h_jetsDeltaPt_->Fill(eventData_.jetsDeltaPt_);
-  histos_.h_massDijets_->Fill(eventData_.massDijets_);
-  histos_.h_MxFromJets_->Fill(eventData_.MxFromJets_);
-  histos_.h_RjjFromJets_->Fill(eventData_.RjjFromJets_);
-  histos_.h_RjjFromPFCands_->Fill(eventData_.RjjFromPFCands_);
-  histos_.h_massDijetsGen_->Fill(eventData_.massDijetsGen_);
-  histos_.h_leadingJetBDiscriminator_->Fill(eventData_.leadingJetBDiscriminator_);
-  histos_.h_secondJetBDiscriminator_->Fill(eventData_.secondJetBDiscriminator_);
+  histosTH1F_["leadingJetPt"]->Fill(eventData_.leadingJetPt_);
+  histosTH1F_["secondJetPt"]->Fill(eventData_.secondJetPt_);
+  histosTH1F_["leadingJetEta"]->Fill(eventData_.leadingJetEta_);
+  histosTH1F_["secondJetEta"]->Fill(eventData_.secondJetEta_);
+  histosTH1F_["leadingJetPhi"]->Fill(eventData_.leadingJetPhi_);
+  histosTH1F_["secondJetPhi"]->Fill(eventData_.secondJetPhi_);  
+  histosTH1F_["leadingJetBDiscriminator"]->Fill(eventData_.leadingJetBDiscriminator_);
+  histosTH1F_["secondJetBDiscriminator"]->Fill(eventData_.secondJetBDiscriminator_);
 
-  histos_.h_trackMultiplicity_->Fill(eventData_.trackMultiplicity_);
-  histos_.h_multiplicityHFPlus_->Fill(eventData_.multiplicityHFPlus_);
-  histos_.h_multiplicityHFMinus_->Fill(eventData_.multiplicityHFMinus_); 
+  histosTH1F_["thirdJetPt"]->Fill(eventData_.thirdJetPt_);
+  histosTH1F_["thirdJetEta"]->Fill(eventData_.thirdJetEta_);
+  histosTH1F_["jetsAveEta"]->Fill(eventData_.jetsAveEta_);
+  histosTH1F_["jetsDeltaEta"]->Fill(eventData_.jetsDeltaEta_);
+  histosTH1F_["jetsDeltaPhi"]->Fill(eventData_.jetsDeltaPhi_);
+  histosTH1F_["jetsDeltaPt"]->Fill(eventData_.jetsDeltaPt_);
+
+  histosTH1F_["massDijets"]->Fill(eventData_.massDijets_);
+  histosTH1F_["massDijetsGen"]->Fill(eventData_.massDijetsGen_);
+  histosTH1F_["MxFromJets"]->Fill(eventData_.MxFromJets_);
+  histosTH1F_["RjjFromJets"]->Fill(eventData_.RjjFromJets_);
+  histosTH1F_["RjjFromPFCands"]->Fill(eventData_.RjjFromPFCands_);
+
+  histosTH1F_["trackMultiplicity"]->Fill(eventData_.trackMultiplicity_);
+  histosTH1F_["multiplicityHFPlus"]->Fill(eventData_.multiplicityHFPlus_);
+  histosTH1F_["multiplicityHFMinus"]->Fill(eventData_.multiplicityHFMinus_); 
   
-  histos_.h_xiGenPlus_->Fill(eventData_.xiGenPlus_);
-  histos_.h_xiGenMinus_->Fill(eventData_.xiGenMinus_);
-  histos_.h_xiTowerPlus_->Fill(eventData_.xiTowerPlus_);
-  histos_.h_xiTowerMinus_->Fill(eventData_.xiTowerMinus_);
-  histos_.h_xiPlusFromJets_->Fill(eventData_.xiPlusFromJets_);
-  histos_.h_xiMinusFromJets_->Fill(eventData_.xiMinusFromJets_);
-  histos_.h_xiPlusFromPFCands_->Fill(eventData_.xiPlusFromPFCands_);
-  histos_.h_xiMinusFromPFCands_->Fill(eventData_.xiMinusFromPFCands_);
-  histos_.h_missingMassFromXi_->Fill(eventData_.missingMassFromXi_);
+  histosTH1F_["xiGenPlus"]->Fill(eventData_.xiGenPlus_);
+  histosTH1F_["xiGenMinus"]->Fill(eventData_.xiGenMinus_);
+  histosTH1F_["xiTowerPlus"]->Fill(eventData_.xiTowerPlus_);
+  histosTH1F_["xiTowerMinus"]->Fill(eventData_.xiTowerMinus_);
+  histosTH1F_["xiPlusFromJets"]->Fill(eventData_.xiPlusFromJets_);
+  histosTH1F_["xiMinusFromJets"]->Fill(eventData_.xiMinusFromJets_);
+  histosTH1F_["xiPlusFromPFCands"]->Fill(eventData_.xiPlusFromPFCands_);
+  histosTH1F_["xiMinusFromPFCands"]->Fill(eventData_.xiMinusFromPFCands_);
+  histosTH1F_["missingMassFromXi"]->Fill(eventData_.missingMassFromXi_);
 
   double resMassDijets = (eventData_.massDijets_ - eventData_.massDijetsGen_)/eventData_.massDijetsGen_;
-  histos_.h_ResMassDijets_->Fill(resMassDijets);
-  histos_.h_RjjFromJetsVsBDicriminator_->Fill(eventData_.RjjFromJets_,eventData_.leadingJetBDiscriminator_);
-  histos_.h_RjjFromPFCandsVsBDicriminator_->Fill(eventData_.RjjFromPFCands_,eventData_.leadingJetBDiscriminator_);
-
-  //histos_.h_iEtaVsHFCountPlus_->Fill(ieta,nHFPlus_ieta);
-  //histos_.h_iEtaVsHFCountMinus_->Fill(ieta,nHFMinus_ieta);
+  histosTH1F_["ResMassDijets"]->Fill(resMassDijets);
 
   double resXiTowerPlus = (eventData_.xiTowerPlus_ - eventData_.xiGenPlus_)/eventData_.xiGenPlus_;
-  histos_.h_ResXiTowerPlus_->Fill(resXiTowerPlus);
+  histosTH1F_["ResXiTowerPlus"]->Fill(resXiTowerPlus);
   double resXiTowerMinus = (eventData_.xiTowerMinus_ - eventData_.xiGenMinus_)/eventData_.xiGenMinus_; 
-  histos_.h_ResXiTowerMinus_->Fill(resXiTowerMinus);
+  histosTH1F_["ResXiTowerMinus"]->Fill(resXiTowerMinus);
   double resXiPlusFromJets = (eventData_.xiPlusFromJets_ - eventData_.xiGenPlus_)/eventData_.xiGenPlus_;
-  histos_.h_ResXiPlusFromJets_->Fill(resXiPlusFromJets);
+  histosTH1F_["ResXiPlusFromJets"]->Fill(resXiPlusFromJets);
   double resXiMinusFromJets = (eventData_.xiMinusFromJets_ - eventData_.xiGenMinus_)/eventData_.xiGenMinus_;
-  histos_.h_ResXiMinusFromJets_->Fill(resXiMinusFromJets);
+  histosTH1F_["ResXiMinusFromJets"]->Fill(resXiMinusFromJets);
   double resXiPlusFromPFCands = (eventData_.xiPlusFromPFCands_ - eventData_.xiGenPlus_)/eventData_.xiGenPlus_;
-  histos_.h_ResXiPlusFromPFCands_->Fill(resXiPlusFromPFCands);
+  histosTH1F_["ResXiPlusFromPFCands"]->Fill(resXiPlusFromPFCands);
   double resXiMinusFromPFCands = (eventData_.xiMinusFromPFCands_ - eventData_.xiGenMinus_)/eventData_.xiGenMinus_;
-  histos_.h_ResXiMinusFromPFCands_->Fill(resXiMinusFromPFCands);
+  histosTH1F_["ResXiMinusFromPFCands"]->Fill(resXiMinusFromPFCands);
 
-  histos_.h_xiPlusVsxiGenPlus_->Fill(eventData_.xiGenPlus_,eventData_.xiPlusFromPFCands_);
-  histos_.h_xiMinusVsxiGenMinus_->Fill(eventData_.xiGenMinus_,eventData_.xiMinusFromPFCands_);
+  histosTH2F_["RjjFromJetsVsBDicriminator"]->Fill(eventData_.RjjFromJets_,eventData_.leadingJetBDiscriminator_);
+  histosTH2F_["RjjFromPFCandsVsBDicriminator"]->Fill(eventData_.RjjFromPFCands_,eventData_.leadingJetBDiscriminator_);
+
+  //histosTH2F_["iEtaVsHFCountPlus"]->Fill(ieta,nHFPlus_ieta);
+  //histosTH2F_["iEtaVsHFCountMinus"]->Fill(ieta,nHFMinus_ieta);
+
+  histosTH2F_["xiPlusVsxiGenPlus"]->Fill(eventData_.xiGenPlus_,eventData_.xiPlusFromPFCands_);
+  histosTH2F_["xiMinusVsxiGenMinus"]->Fill(eventData_.xiGenMinus_,eventData_.xiMinusFromPFCands_);
 
   // Fill TTree
   if(saveTTree_) data_->Fill();
