c79311af45bd283730c795c17c574e077b3b0b64
antoniov <>
2009-11-20 16:41:02 +0000

    update

--- ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/python/analysisTools.py
+++ ExclusiveDijetsAnalysis/ExclusiveDijetsAnalysis/python/analysisTools.py
@@ -4,9 +4,9 @@ def parseInput(inputFields,requiredFields = ()):
 
     class options: pass
     for item in sys.argv:
-        option = item.split('=')[0]
+        option = item.split('=')[0].strip()
         if option in inputFields:
-            value = item.split('=')[1]
+            value = item.split('=')[1].strip()
             if value in ('true','True','yes','Yes'): value = True
             elif value in ('false','False','no','No'): value = False
 
