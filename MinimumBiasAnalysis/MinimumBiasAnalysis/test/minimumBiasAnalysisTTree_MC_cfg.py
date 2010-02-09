aa6c8c60086427aeb5e75346448aeaa948e4478c
antoniov <>
2010-02-09 21:00:01 +0000

    update

--- MinimumBiasAnalysis/MinimumBiasAnalysis/test/minimumBiasAnalysisTTree_MC_cfg.py
+++ MinimumBiasAnalysis/MinimumBiasAnalysis/test/minimumBiasAnalysisTTree_MC_cfg.py
@@ -7,6 +7,9 @@ process.source = cms.Source("PoolSource",
 )
 from fileNames_MinBias_STARTUP3X_V8P_900GeV_Jan29ReReco import fileNames
 process.source.fileNames = fileNames
+process.GlobalTag.globaltag = 'STARTUP3X_V8P::All'
+process.minimumBiasTTreeAnalysis_eventSelectionMinBiasBSCOR.AccessMCInfo = True
+process.minimumBiasTTreeAnalysis_eventSelectionMinBiasPixel.AccessMCInfo = True
 
 for pathname in process.paths:
     getattr(process,pathname).remove(getattr(process,'bptx'))
