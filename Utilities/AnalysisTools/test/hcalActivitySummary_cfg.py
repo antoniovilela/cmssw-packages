a3122f80e35e1d678f44fc2d1ea752121d4cd56c
antoniov <>
2010-07-27 18:41:54 +0000

    commit of HCALActivityFilter

--- Utilities/AnalysisTools/test/hcalActivitySummary_cfg.py
+++ Utilities/AnalysisTools/test/hcalActivitySummary_cfg.py
@@ -13,15 +13,24 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:/tmp/antoniov/MinimumBias_Commissioning10_GOODCOLL-Jun14thSkim_v1_CA78CDED-2E83-DF11-BDD8-0026189438A7.root')
 )
 
-process.load('Utilities.AnalysisTools.hcalActivitySummary_cfi')
-
 process.output = cms.OutputModule("PoolOutputModule",
     outputCommands = cms.untracked.vstring('drop *','keep *_*_*_Analysis'),
     fileName = cms.untracked.string('/tmp/antoniov/hcalActivitySummary.root'),
     SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('analysis_step')
+        SelectEvents = cms.vstring('reco_step')
     )
 )
 
-process.analysis_step = cms.Path(process.hcalActivitySummary)
+process.load('Utilities.AnalysisTools.hcalActivitySummary_cfi')
+process.load('Utilities.AnalysisTools.hcalActivityFilter_cfi')
+process.hcalActivityFilter.EnergyThresholdHB = 1.5
+process.hcalActivityFilter.EnergyThresholdHE = 1.5
+process.hcalActivityFilter.EnergyThresholdHF = 4.0
+process.hcalActivityFilter.NTowersMaxHEPlus = 0
+process.hcalActivityFilter.NTowersMaxHFPlus = 0
+#process.hcalActivityFilter.SumEMaxHFPlus = 8.0
+#process.hcalActivityFilter.SumEMaxHEPlus = 4.0
+
+process.reco_step = cms.Path(process.hcalActivitySummary)
+process.analysis_step = cms.Path(process.hcalActivityFilter)
 process.out_step = cms.EndPath(process.output)
