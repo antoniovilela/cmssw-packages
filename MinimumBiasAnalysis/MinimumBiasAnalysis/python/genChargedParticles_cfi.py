28a835bb0c764875fa66051841ef4b1e7309a210
antoniov <>
2010-11-03 18:44:30 +0000

    update

--- MinimumBiasAnalysis/MinimumBiasAnalysis/python/genChargedParticles_cfi.py
+++ MinimumBiasAnalysis/MinimumBiasAnalysis/python/genChargedParticles_cfi.py
@@ -3,5 +3,5 @@ import FWCore.ParameterSet.Config as cms
 genChargedParticles = cms.EDFilter("GenParticleSelector",
     filter = cms.bool(False),
     src = cms.InputTag("genParticles"),
-    cut = cms.string('charge != 0 & pt > 0.5 & status = 1')
+    cut = cms.string('charge != 0 & pt > 0.5 & abs(eta) < 2.4 & status = 1')
 )
