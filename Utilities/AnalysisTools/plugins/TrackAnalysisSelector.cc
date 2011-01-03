7b958893328afe89c3453872f3d6215971085822
antoniov <>
2011-01-03 17:39:01 +0000

    move TrackAnalysisSelector to AnalysisTools

--- Utilities/AnalysisTools/plugins/TrackAnalysisSelector.cc
+++ Utilities/AnalysisTools/plugins/TrackAnalysisSelector.cc
@@ -1,12 +1,12 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "CommonTools/RecoAlgos/interface/TrackFullCloneSelectorBase.h"
-#include "MinimumBiasAnalysis/MinimumBiasAnalysis/interface/TrackAnalysisSelector.h"
+#include "Utilities/AnalysisTools/interface/TrackAnalysisSelector.h"
 
 namespace reco { 
   namespace modules {
 
-    typedef TrackFullCloneSelectorBase< minimumBiasAnalysis::TrackAnalysisSelector > TrackAnalysisSelector;
+    typedef TrackFullCloneSelectorBase< analysisTools::TrackAnalysisSelector > TrackAnalysisSelector;
 
     DEFINE_FWK_MODULE(TrackAnalysisSelector);
   }
