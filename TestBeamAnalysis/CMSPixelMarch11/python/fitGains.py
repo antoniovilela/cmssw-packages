81d9484d422a3c6c8f4621a1a895a4aa1a14277d
antoniov <>
2011-07-01 17:57:55 +0000

    update

--- TestBeamAnalysis/CMSPixelMarch11/python/fitGains.py
+++ TestBeamAnalysis/CMSPixelMarch11/python/fitGains.py
@@ -1,7 +1,96 @@
 import ROOT
 import os,sys
 
-def fitGains(rootFileName,outputFileName,stationId,plaqId,chipId,verbose=True):
+def fitLinear(file,stationId,plaqId,chipId,row,col,option=""):
+    minFracHits = 0.90
+    minFracADC = 0.85
+
+    h_hits = file.Get( "hits_%d_%d" % (row,col) )
+    h_adc = file.Get( "adc_%d_%d" % (row,col) )
+
+    def defaultWord():
+        word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,0.0,0.0,0.0)
+        return word
+
+    maxHits = h_hits.GetMaximum()
+    if maxHits == 0: return defaultWord()
+
+    # Find linear fit range 
+    iBinMin = -1
+    for ibin in range( 1, (h_hits.GetNbinsX()+1) ):
+        if (h_hits.GetBinContent(ibin)/maxHits) >= minFracHits:
+            iBinMin = ibin
+            break
+
+    minADC = h_adc.GetBinContent(iBinMin)
+    maxADC = h_adc.GetMaximum()
+    if maxADC == 0 or (maxADC - minADC) == 0: return defaultWord()
+
+    iBinMax = -1
+    for ibin in range( 1, (h_adc.GetNbinsX()+1) ):
+        if ( (h_adc.GetBinContent(ibin) - minADC)/(maxADC - minADC) ) >= minFracADC:
+            iBinMax = ibin
+            break
+
+    minFit = h_adc.GetBinLowEdge(iBinMin)
+    maxFit = h_adc.GetBinLowEdge(iBinMax)
+    fitFunc = ROOT.TF1("fit_adc_%d_%d" % (row,col),"pol1",minFit,maxFit)
+    h_adc.Fit(fitFunc,"R" + option)
+    fit_slope = fitFunc.GetParameter(1)
+    fit_ped = fitFunc.GetParameter(0)
+    if fit_slope <= 0: return defaultWord()
+
+    # Format output
+    word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,fit_slope,fit_ped,maxADC)
+    return word
+
+def fitTgh(file,stationId,plaqId,chipId,row,col,option=""):
+    minFracHits = 0.10
+
+    h_hits = file.Get( "hits_%d_%d" % (row,col) )
+    h_adc = file.Get( "adc_%d_%d" % (row,col) )
+     
+    def defaultWord():
+        word = "%d %d %d %d %d %.3f %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,-1.,-1.,-1.,-1.)
+        return word
+
+    maxHits = h_hits.GetMaximum()
+    if maxHits == 0:
+        #print "Row ",row,"Col ",col,"has zero hits"
+        return defaultWord()
+
+    iBinMin = -1
+    for ibin in range( 1, (h_hits.GetNbinsX()+1) ):
+        if (h_hits.GetBinContent(ibin)/maxHits) >= minFracHits:
+            iBinMin = ibin
+            break
+
+    iBinMax = h_adc.GetMaximumBin()
+    #print "Fit from bin ",iBinMin,"to bin  ",iBinMax
+    minFit = h_adc.GetBinLowEdge(iBinMin)
+    maxFit = h_adc.GetBinLowEdge(iBinMax)
+    # Define fit function
+    fitFunc = ROOT.TF1("fit_adc_%d_%d" % (row,col),"[0]+[1]*TMath::TanH([2]*x+[3])",minFit,maxFit)
+    fitFunc.SetParameter(2,0.00382)
+    fitFunc.SetParameter(3,-0.886)
+    fitFunc.SetParameter(1,112.7)
+    fitFunc.SetParameter(0,113.0)
+    h_adc.Fit(fitFunc,"R" + option)
+    if fitFunc.GetParameter(3) == -0.886:
+         fit_p0 = -1.0
+         fit_p1 = -1.0
+         fit_p2 = -1.0
+         fit_p3 = -1.0
+    else:
+         fit_p0 = fitFunc.GetParameter(0)
+         fit_p1 = fitFunc.GetParameter(1)
+         fit_p2 = fitFunc.GetParameter(2)
+         fit_p3 = fitFunc.GetParameter(3)
+
+    word = "%d %d %d %d %d %.3f %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,fit_p0,fit_p1,fit_p2,fit_p3)
+    return word
+
+def fitGains(rootFileName,outputFileName,stationId,plaqId,chipId,mode='linear',verbose=True):
     file = ROOT.TFile(rootFileName,'READ')
     outFile = open(outputFileName,'w')
 
@@ -17,53 +106,16 @@ def fitGains(rootFileName,outputFileName,stationId,plaqId,chipId,verbose=True):
 
     nRows = 80
     nCols = 52
-    minFracHits = 0.90
-    minFracADC = 0.85
-    outFile.write("st pl ch r c slope pedestal max\n")
+    if mode == 'linear': outFile.write("st pl ch r c slope pedestal max\n")
+    elif mode == 'tgh': outFile.write("st pl ch r c p0 p1 p2 p3\n")
     for row in range(nRows):
         for col in range(nCols):
-            h_hits = file.Get( "hits_%d_%d" % (row,col) )
-            h_adc = file.Get( "adc_%d_%d" % (row,col) )
-            # Find linear fit range
-            maxHits = h_hits.GetMaximum()
-            if maxHits == 0:
-                word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,0.0,0.0,0.0)
+            if mode == 'linear':
+                word = fitLinear(file,stationId,plaqId,chipId,row,col,option)
                 writeWord(word)
-                continue
- 
-            iBinMin = -1
-            for ibin in range( 1, (h_hits.GetNbinsX()+1) ):
-                if (h_hits.GetBinContent(ibin)/maxHits) >= minFracHits:
-                    iBinMin = ibin
-                    break
-    
-            minADC = h_adc.GetBinContent(iBinMin)
-            maxADC = h_adc.GetMaximum()
-            if maxADC == 0 or (maxADC - minADC) == 0: 
-                word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,0.0,0.0,0.0)
+            elif mode == 'tgh':
+                word = fitTgh(file,stationId,plaqId,chipId,row,col,option)
                 writeWord(word)
-                continue
-
-            iBinMax = -1
-            for ibin in range( 1, (h_adc.GetNbinsX()+1) ):
-                if ( (h_adc.GetBinContent(ibin) - minADC)/(maxADC - minADC) ) >= minFracADC:
-                    iBinMax = ibin
-                    break
-
-            minFit = h_adc.GetBinLowEdge(iBinMin)
-            maxFit = h_adc.GetBinLowEdge(iBinMax)
-            fitFunc = ROOT.TF1("fit_adc_%d_%d" % (row,col),"pol1",minFit,maxFit)
-            h_adc.Fit(fitFunc,"R" + option)
-            fit_slope = fitFunc.GetParameter(1)
-            fit_ped = fitFunc.GetParameter(0)
-            if fit_slope <= 0:
-                word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,0.0,0.0,0.0)
-                writeWord(word)
-                continue
-
-            # Write output
-            word = "%d %d %d %d %d %.3f %.3f %.3f\n" % (stationId,plaqId,chipId,row,col,fit_slope,fit_ped,maxADC)
-            writeWord(word)
 
     file.Close()
     outFile.close()
@@ -72,6 +124,7 @@ if __name__ == '__main__':
     import optparse
     parser = optparse.OptionParser(usage="usage: %prog [options] <file name>")
     parser.add_option("-o","--out", dest="out", help="Output file name")
+    parser.add_option("-m","--mode", dest="mode", default='linear', help="Fit mode: 'linear' or 'tgh' (default: linear)")
     parser.add_option("-s","--station", dest="station", type='int', default=0, help="Station Id")
     parser.add_option("-p","--plaq", dest="plaq", type='int', default=0, help="Plaquette Id")
     parser.add_option("-c","--chip", dest="chip", type='int', default=0, help="Chip Id")
@@ -89,6 +142,7 @@ if __name__ == '__main__':
     fitGains(inputFile,outFile,
                        stationId=input.station,
                        plaqId=input.plaq,
-                       chipId=input.chip)
+                       chipId=input.chip,
+                       mode=input.mode)
 
     sys.exit(0)
--- TestBeamAnalysis/CMSPixelMarch11/python/writeGains.py
+++ TestBeamAnalysis/CMSPixelMarch11/python/writeGains.py
@@ -6,13 +6,17 @@ if __name__ == '__main__':
     parser = optparse.OptionParser(usage="usage: %prog [options]")
     parser.add_option("-d","--dir", dest="dir", help="Dir with calibration ROOT files")
     parser.add_option("-o","--out", dest="out", help="Dir with output files")
+    parser.add_option("-m","--mode", dest="mode", default='linear', help="Fit mode: 'linear' or 'tgh' (default: linear)")
     parser.add_option("-t","--type", dest="type", default="TELESCOPE", help="Uses file names matching TYPE (Default: 'TELESCOPE')")
+    parser.add_option("-v","--verbose", action="store_true", dest="verbose", default=False, help="Verbose mode (default = False)")
 
     (input, args) = parser.parse_args()
 
     fileNames = os.listdir(input.dir)
     fileNames = [ '%s/%s' % (input.dir,item) for item in fileNames if item.find(input.type) != -1 ]
 
+    verbose = input.verbose
+    mode = input.mode
     outDir = ''
     if hasattr(input,'out'): outDir = input.out
     for file in fileNames:
@@ -21,11 +25,11 @@ if __name__ == '__main__':
         (st,pl,ch) = file.split('.')[0].split('_')[-3:]         
         print "St=%d Pl=%d Ch=%d" % (int(st),int(pl)/10,int(ch)) 
         print file, outFile
-        """
         fitGains(file,outFile,
                       stationId=int(st),
                       plaqId=int(pl)/10,
                       chipId=int(ch),
-                      verbose=False)
-        """
+                      mode=mode,
+                      verbose=verbose)
+
     sys.exit(0)
