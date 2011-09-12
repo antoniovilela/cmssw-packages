a05aaa820c2661c20b574d8c760c46a8c8320183
antoniov <>
2011-09-12 20:30:36 +0000

    update

--- MinimumBiasAnalysis/MinimumBiasAnalysis/test/plotPFlowNoise.py
+++ MinimumBiasAnalysis/MinimumBiasAnalysis/test/plotPFlowNoise.py
@@ -1,4 +1,13 @@
 import ROOT
+from MinimumBiasAnalysis.MinimumBiasAnalysis.pfThresholds_cfi import pfThresholds
+
+"""
+def getThreshold(pset,region,type,var):
+    regionPSet = getattr(pset,region)
+    typePSet = getattr(regionPSet,type)
+    value = getattr(typePSet,var).value()
+    return value
+"""
 
 def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""):
 
@@ -6,18 +15,6 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
     ROOT.TH1.AddDirectory(False)
     
     file = ROOT.TFile(fileName,'READ')
-
-    """
-    h_energyVsEtaAllTypes
-    h_energyVsEtaUndefined
-    h_energyVsEtaChargedHadron
-    h_energyVsEtaElectron
-    h_energyVsEtaMuon
-    h_energyVsEtaGamma
-    h_energyVsEtaNeutralHadron
-    h_energyVsEtaHadronHF
-    h_energyVsEtaEGammaHF
-    """
 
     particleTypes = ("all","gamma","hadronNeutral","hadronHF","emHF")
     histoNames = {}
@@ -31,6 +28,7 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
     regions["Endcap"] = (1.4,2.6) 
     regions["Transition"] = (2.6,3.2)
     regions["Forward"] = (3.2,5.205)
+    """
     thresholds = {}
     thresholds["all"] = {}
     thresholds["gamma"] = {"Barrel":0.9,
@@ -42,11 +40,24 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
     thresholds["hadronHF"] = {"Transition":4.0,
                               "Forward":4.0}
     thresholds["emHF"] = {"Transition":3.5,
-                          "Forward":3.5} 
+                          "Forward":3.5}
+    """
+    thresholds = {}
+    for type in particleTypes:
+        thresholds[type] = {} 
+        if type == 'all': continue
+        for region in regions:
+	    regionPSet = getattr(pfThresholds,region)
+            if hasattr(regionPSet,type):
+		typePSet = getattr(regionPSet,type)
+                if hasattr(typePSet,'energy'):
+		    value = getattr(typePSet,'energy').value()
+		    thresholds[type][region] = value
 
     canvases = []
     histos = []
     objects = []
+    thresholdsNew = {}
     for type in particleTypes:
         histo = file.Get("%s/%s" % (selection,histoNames[type]))
         canvas = ROOT.TCanvas("c_" + histo.GetName())
@@ -64,9 +75,11 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
         canvasesProj = []         
         projections = []
         linesProj = []
+        thresholdsNew[type] = {}
         for region in thresholds[type]:
             xrange = regions[region]
             ythreshold = thresholds[type][region]
+            print "Threshold for %s, %s region, eta=[%.1f,%.1f): %.1f" % (type,region,xrange[0],xrange[1],ythreshold)
 
             canvas.cd()
             lines.append(ROOT.TLine(xrange[0],ythreshold,xrange[1],ythreshold))
@@ -89,13 +102,32 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
 
             iBinLow = histo.GetXaxis().FindBin(xrange[0])
             iBinUpp = histo.GetXaxis().FindBin(xrange[1]) - 1
-            print "Bins range for %s, %s region, eta=[%.1f,%.1f): [%d,%d]" % (type,region,xrange[0],xrange[1],iBinLow,iBinUpp)
+            print "Bin range for %s, %s region, eta=[%.1f,%.1f): [%d,%d]" % (type,region,xrange[0],xrange[1],iBinLow,iBinUpp)
             projectionY = histo.ProjectionY(histo.GetName() + "_proj_" + region + "_zplus", iBinLow, iBinUpp)
             projections.append(projectionY)
             projections[-1].SetMarkerStyle(20)
             projections[-1].SetMarkerSize(1.2) 
             projections[-1].Rebin(rebin)
-            projections[-1].DrawNormalized() 
+            projections[-1].Scale( 1./projections[-1].GetSumOfWeights() )
+            ###
+            nSigmas = 4
+            fitOption = "R"
+            iBinMax = projections[-1].GetMaximumBin()
+            minFit = projections[-1].GetBinCenter(iBinMax) - projections[-1].GetRMS()
+            maxFit = projections[-1].GetBinCenter(iBinMax) + projections[-1].GetRMS()
+
+	    funcName = projections[-1].GetName() + "_gaus"
+	    fitFunc = ROOT.TF1(funcName,"gaus",minFit,maxFit)
+	    projections[-1].Fit(fitFunc,fitOption)
+
+	    minFit = fitFunc.GetParameter(1) - nSigmas*fitFunc.GetParameter(2)
+	    maxFit = fitFunc.GetParameter(1) + nSigmas*fitFunc.GetParameter(2)
+	    fitFunc.SetRange(minFit,maxFit)
+	    projections[-1].Fit(fitFunc,fitOption)
+            thresholdsNew[type][region] = fitFunc.GetParameter(1) + 5*fitFunc.GetParameter(2)  
+            ###
+            #projections[-1].DrawNormalized() 
+            projections[-1].Draw()
    
             linesProj.append(ROOT.TLine(ythreshold,0.,
                                         ythreshold,0.15))
@@ -112,7 +144,7 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
 
             iBinLow = histo.GetXaxis().FindBin(-xrange[1]) + 1
             iBinUpp = histo.GetXaxis().FindBin(-xrange[0])
-            print "Bins range for %s, %s region, eta=(%.1f,%.1f]: [%d,%d]" % (type,region,-xrange[1],-xrange[0],iBinLow,iBinUpp)
+            print "Bin range for %s, %s region, eta=(%.1f,%.1f]: [%d,%d]" % (type,region,-xrange[1],-xrange[0],iBinLow,iBinUpp)
             projectionY = histo.ProjectionY(histo.GetName() + "_proj_" + region + "_zminus", iBinLow, iBinUpp)
             projections.append(projectionY)
             projections[-1].SetMarkerStyle(20)
@@ -135,6 +167,14 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
         objects.append(linesProj)
 
     ROOT.TH1.AddDirectory(AddDirectoryStatus_)
+
+    for type in particleTypes:
+        for region in thresholds[type]:
+            xrange = regions[region]
+            ythreshold = thresholds[type][region]
+            ythresholdNew = thresholdsNew[type][region]
+            print "New threshold for %s, %s region, eta=[%.1f,%.1f): %.2f --> %.2f" % (type,region,xrange[0],xrange[1],ythreshold,ythresholdNew)
+
     if outputFileName:
         outputFile = ROOT.TFile(outputFileName,'recreate')
         outputFile.cd()
