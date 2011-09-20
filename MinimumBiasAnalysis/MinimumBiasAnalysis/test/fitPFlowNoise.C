b86fed0c80507c4d9f674fe7abe454e7d0e5ddf5
antoniov <>
2011-09-20 20:51:42 +0000

    update

--- MinimumBiasAnalysis/MinimumBiasAnalysis/test/fitPFlowNoise.C
+++ MinimumBiasAnalysis/MinimumBiasAnalysis/test/fitPFlowNoise.C
@@ -1,6 +1,7 @@
 #if !defined(__CINT__) || defined(__MAKECINT__)
 #include <TFile.h>
 #include <TCanvas.h>
+#include <TLegend.h>
 #include <TH1.h>
 
 #include "RooRealVar.h"
@@ -14,7 +15,6 @@
 #include <vector>
 #include <map>
 #include <iostream>
-
 #endif
 
 using namespace RooFit;
@@ -46,6 +46,7 @@ void fitPFlowNoise(std::string const& fileName){
 
    std::map<std::string,double> thresholds;
    std::vector<TCanvas*> canvases;
+   std::vector<TLegend*> legends;
    std::vector<std::string>::const_iterator var = variables.begin();
    std::vector<std::string>::const_iterator vars_end = variables.end();
    for(;var != vars_end; ++var){
@@ -55,47 +56,79 @@ void fitPFlowNoise(std::string const& fileName){
       for(int ibin = 1; ibin <= iBinMax; ++ibin){
          if( histo->GetBinContent(ibin) >= 0.15*histo->GetMaximum() ) {iBinLow = ibin; break;}
       }
-    
-      double minHisto = histo->GetBinCenter(1);
-      double maxHisto = histo->GetBinCenter( histo->GetNbinsX() );
-      double rangeFitLow = histo->GetBinCenter(iBinLow);
-      double rangeFitHigh = histo->GetBinCenter( histo->GetNbinsX() );
-      std::cout << "Histogram range, maximum" << minHisto << ", " << maxHisto << ", " 
-                                              << histo->GetBinCenter(iBinMax) << ", " << histo->GetMaximum() << std::endl;
-      std::cout << "Fit range"  << rangeFitLow << ", " << rangeFitHigh << std::endl;
-  
-      RooRealVar x((std::string(histo->GetName()) + "_var").c_str(),"energy",minHisto,maxHisto);
-      RooRealVar sigma1("sigma1","sigma1",0.,0.7);
-      RooRealVar sigma2("sigma2","sigma2",0.,0.7);
-      RooRealVar mean1("mean1","mean1",histo->GetBinCenter(iBinMax),rangeFitLow,rangeFitHigh);
-      RooRealVar mean2("mean2","mean2",histo->GetBinCenter(iBinMax),rangeFitLow,rangeFitHigh);
 
-      RooRealVar frac("frac","frac",0.,1.);
+      double maxPeak = histo->GetBinCenter(iBinMax);
+    
+      double minHisto = histo->GetBinLowEdge(1);
+      double maxHisto = histo->GetBinLowEdge(histo->GetNbinsX()) + histo->GetBinWidth(histo->GetNbinsX());
+
+      double rangeFitLow = histo->GetBinLowEdge(iBinLow);
+      double rangeFitHigh = maxHisto;
+
+      std::cout << "Histogram range, maximum " << minHisto << ", " << maxHisto << ", " 
+                                               << maxPeak << ", "
+                                               << histo->GetMaximum() << std::endl;
+      std::cout << "Fit limits "  << rangeFitLow << ", " << rangeFitHigh << std::endl;
+
+      RooRealVar x((std::string(histo->GetName()) + "_var").c_str(),"energy",minHisto,maxHisto);
+      RooDataHist hdata((std::string(histo->GetName()) + "_hdata").c_str(),"Binned data",RooArgList(x),histo);
+
+      // First fit single Gaussian around peak
+      double initRMS = histo->GetRMS();
+      if(initRMS < histo->GetBinWidth(iBinMax)) initRMS = histo->GetBinWidth(iBinMax);
+  
+      double minFit = maxPeak - 2*initRMS;
+      if(minFit < rangeFitLow) minFit = rangeFitLow; 
+      double maxFit = maxPeak + 2*initRMS;
+      std::cout << "Fit range "  << minFit << ", " << maxFit << std::endl;
+
+      RooRealVar mean_singleGaus("mean_singleGaus","mean_singleGaus",maxPeak,minFit,maxFit);
+      RooRealVar sigma_singleGaus("sigma_singleGaus","sigma_singleGaus",initRMS,0.,5.0);
+      RooGaussian singleGaus((std::string(histo->GetName()) + "_singleGaus").c_str(),"Gaussian distribution",x,mean_singleGaus,sigma_singleGaus);
+      singleGaus.fitTo(hdata,Range(minFit,maxFit));
+
+      // Double gaussian
+      minFit = mean_singleGaus.getVal() - 3*sigma_singleGaus.getVal();
+      if(minFit < rangeFitLow) minFit = rangeFitLow;
+      maxFit = mean_singleGaus.getVal() + 15*sigma_singleGaus.getVal();
+      std::cout << "Fit range "  << minFit << ", " << maxFit << std::endl;
+
+      RooRealVar sigma1("sigma1","sigma1",sigma_singleGaus.getVal(),0.,0.1);
+      RooRealVar sigma2("sigma2","sigma2",5*sigma_singleGaus.getVal(),0.,5.0);
+      RooRealVar mean1("mean1","mean1",mean_singleGaus.getVal(),minFit,maxFit);
+      RooRealVar mean2("mean2","mean2",mean_singleGaus.getVal(),minFit,maxFit);
+      RooRealVar frac("frac","frac",0.25,0.,1.);
 
       RooGaussian gaus1((std::string(histo->GetName()) + "_gaus1").c_str(),"Gaussian distribution",x,mean1,sigma1);
       RooGaussian gaus2((std::string(histo->GetName()) + "_gaus2").c_str(),"Gaussian distribution",x,mean2,sigma2);
       RooAddPdf model((std::string(histo->GetName()) + "_model").c_str(),"Model",RooArgList(gaus1,gaus2),RooArgList(frac));
 
-      RooDataHist hdata((std::string(histo->GetName()) + "_hdata").c_str(),"Binned data",RooArgList(x),histo);
       //model.fitTo( hdata,ROOT.RooFit.Minos(0) )
       //model.fitTo(hdata,ROOT.RooFit.Minos(0),ROOT.RooFit.Range(rangeFitLow,rangeFitHigh))
-      //model.fitTo(hdata,ROOT.RooFit.Range(rangeFitLow,rangeFitHigh))
-      model.fitTo(hdata);
+      model.fitTo(hdata,Range(minFit,maxFit));
+      //model.fitTo(hdata);
 
       double fitMean = mean1.getVal();
       double fitSigma = sigma1.getVal();
-      double thresholdNew = fitMean + 3*fitSigma;      
+      double thresholdNew = fitMean + 5*fitSigma;      
       thresholds[*var] = thresholdNew;
 
       RooPlot* frame = x.frame();
       hdata.plotOn(frame);
+      //singleGaus.plotOn(frame,LineStyle(3));
       model.plotOn(frame);
       model.plotOn(frame,Components(gaus2),LineColor(2),LineStyle(2));
 
       canvases.push_back( new TCanvas( (std::string(histo->GetName()) + "_canvas").c_str() ) );
       canvases.back()->cd();
       frame->Draw();
+      legends.push_back( new TLegend(0.45,0.55,0.95,0.80) );
+      legends.back()->SetHeader( histo->GetName() );
+      legends.back()->AddEntry(&hdata,"Data","LP");
+      legends.back()->AddEntry(&model,"Fit","LF");
+      legends.back()->SetFillColor( canvases.back()->GetFillColor() );
+      legends.back()->Draw("SAME"); 
    }
 
-   for(size_t k = 0; k < variables.size(); ++k) std::cout << variables[k] << ", " << thresholds[variables[k]] << std::endl;
+   for(size_t k = 0; k < variables.size(); ++k) std::cout << variables[k] << " --> " << thresholds[variables[k]] << std::endl;
 }
--- MinimumBiasAnalysis/MinimumBiasAnalysis/test/plotPFlowNoise.py
+++ MinimumBiasAnalysis/MinimumBiasAnalysis/test/plotPFlowNoise.py
@@ -198,23 +198,22 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
     for type in particleTypes:
         if type == 'all': continue
         histo = file.Get("%s/%s" % (selection,histoNames[type]))
-        #canvas = ROOT.TCanvas("c_" + histo.GetName())
-        #canvas.SetGridy()
-        #canvas.cd() 
+        canvas = ROOT.TCanvas("c_" + histo.GetName())
+        canvas.SetGridy()
+        canvas.cd() 
         
         histo.SetMarkerSize(0.1)
         #histo.SetMarkerColor(4)
         histo.GetXaxis().SetTitle("#eta")
         histo.GetYaxis().SetTitle("Energy (GeV)")
         histo.SetStats(0)
-        #histo.Draw("colz")
+        histo.Draw("colz")
 
         lines = []
         canvasesProj = []         
         projections = []
         linesProj = []
         fitObjects = []
-        frames = []
         thresholdsPlus[type] = {}
         thresholdsMinus[type] = {}
         for region in thresholds[type]:
@@ -222,24 +221,24 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
             ythreshold = thresholds[type][region]
             print "Threshold for %s, %s region, eta=[%.1f,%.1f): %.1f" % (type,region,xrange[0],xrange[1],ythreshold)
 
-            #canvas.cd()
+            canvas.cd()
             lines.append(ROOT.TLine(xrange[0],ythreshold,xrange[1],ythreshold))
             lines[-1].SetLineColor(2)
             lines[-1].SetLineStyle(2)
             lines[-1].SetLineWidth(5)
-            #lines[-1].Draw("SAME")
+            lines[-1].Draw("SAME")
             lines.append(ROOT.TLine(-xrange[1],ythreshold,-xrange[0],ythreshold))
             lines[-1].SetLineColor(2) 
             lines[-1].SetLineStyle(2)
             lines[-1].SetLineWidth(4)
-            #lines[-1].Draw("SAME")
+            lines[-1].Draw("SAME")
 
             # Projections
-            #canvasProj = ROOT.TCanvas("c_" + histo.GetName() + "_proj_" + region + "_zplus")
-            #canvasesProj.append(canvasProj)
-            #canvasesProj[-1].SetGridy()
-            #canvasesProj[-1].SetLogy()
-            #canvasesProj[-1].cd() 
+            canvasProj = ROOT.TCanvas("c_" + histo.GetName() + "_proj_" + region + "_zplus")
+            canvasesProj.append(canvasProj)
+            canvasesProj[-1].SetGridy()
+            canvasesProj[-1].SetLogy()
+            canvasesProj[-1].cd() 
             
             iBinLow = histo.GetXaxis().FindBin(xrange[0])
             iBinUpp = histo.GetXaxis().FindBin(xrange[1]) - 1
@@ -251,75 +250,13 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
             projections[-1].Rebin(rebin)
             projections[-1].Scale( 1./projections[-1].GetSumOfWeights() )
             ###
-            #fitMean,fitSigma = fitHistoDoubleGaus(projections[-1])
-            #thresholdsPlus[type][region] = fitMean + 3*fitSigma  
-            #ythresholdNew = thresholdsPlus[type][region]
-
-            #returnFit = fitHistoRooFit(projections[-1],True) 
-            #fitObjects.append( returnFit )
-            #fitMean,fitSigma = returnFit[0].getVal(),returnFit[2].getVal()
-
-	    iBinMax = projections[-1].GetMaximumBin()
-	    iBinLow = -1
-	    for ibin in range(1,iBinMax + 1):
-		if projections[-1].GetBinContent(ibin) >= 0.15*projections[-1].GetMaximum():
-		    iBinLow = ibin
-		    break
-
-	    minHisto = projections[-1].GetBinCenter(1)
-	    maxHisto = projections[-1].GetBinCenter( projections[-1].GetNbinsX() )
-	    rangeFitLow = projections[-1].GetBinCenter(iBinLow)
-	    rangeFitHigh = projections[-1].GetBinCenter( projections[-1].GetNbinsX() )  
-	    print "Histogram range, maximum",minHisto,maxHisto,projections[-1].GetBinCenter(iBinMax),projections[-1].GetMaximum()
-	    print "Fit range",rangeFitLow,rangeFitHigh
-
-	    x = ROOT.RooRealVar(projections[-1].GetName() + "_var","energy",minHisto,maxHisto)
-	    sigma1 = ROOT.RooRealVar(projections[-1].GetName() + "_sigma1","sigma1",0.,0.7)
-	    sigma2 = ROOT.RooRealVar(projections[-1].GetName() + "_sigma2","sigma2",0.,0.7)
-	    mean1 = ROOT.RooRealVar(projections[-1].GetName() + "_mean1","mean1",projections[-1].GetBinCenter(iBinMax),rangeFitLow,rangeFitHigh)
-	    mean2 = ROOT.RooRealVar(projections[-1].GetName() + "_mean2","mean2",projections[-1].GetBinCenter(iBinMax),rangeFitLow,rangeFitHigh)
-	    
-	    frac = ROOT.RooRealVar(projections[-1].GetName() + "_frac","frac",0.,1.)
-
-	    gaus1 = ROOT.RooGaussian(projections[-1].GetName() + "_gaus1","Gaussian distribution",x,mean1,sigma1)
-	    gaus2 = ROOT.RooGaussian(projections[-1].GetName() + "_gaus2","Gaussian distribution",x,mean2,sigma2)
-	    model = ROOT.RooAddPdf(projections[-1].GetName() + "_model","Model",ROOT.RooArgList(gaus1,gaus2),ROOT.RooArgList(frac))
-
-	    hdata = ROOT.RooDataHist(projections[-1].GetName() + "_hdata","Binned data",ROOT.RooArgList(x),projections[-1])
-	    #model.fitTo( hdata,ROOT.RooFit.Minos(0) )
-	    #model.fitTo(hdata,ROOT.RooFit.Minos(0),ROOT.RooFit.Range(rangeFitLow,rangeFitHigh))
-	    #model.fitTo(hdata,ROOT.RooFit.Range(rangeFitLow,rangeFitHigh))
-	    model.fitTo(hdata)
-            
-            fitMean,fitSigma = mean1.getVal(),sigma1.getVal()
+            fitMean,fitSigma = fitHistoDoubleGaus(projections[-1])
             thresholdsPlus[type][region] = fitMean + 3*fitSigma  
             ythresholdNew = thresholdsPlus[type][region]
-            fitObjects.append([mean1,mean2,sigma1,sigma2,x,hdata,model,gaus1,gaus2])
-
-            #fitDrawObjs = fitObjects[-1][-1]
-	    #frame = fitDrawObjs[0].frame()
-	    #fitDrawObjs[1].plotOn(frame)
-	    #fitDrawObjs[2].plotOn(frame)
-	    #fitDrawObjs[2].plotOn(frame,ROOT.RooFit.Components( fitDrawObjs[4].GetName() ),
-            #                            ROOT.RooFit.LineColor(2),ROOT.RooFit.LineStyle(2))
-
-            frames.append( x.frame() )
-	    hdata.plotOn(frames[-1])
-            #model.plotOn(frame)
-            #model.plotOn(frame,ROOT.RooFit.Components( gaus2.GetName() ),
-            #                   ROOT.RooFit.LineColor(2),ROOT.RooFit.LineStyle(2))
-
-            canvasProj = ROOT.TCanvas("c_" + histo.GetName() + "_proj_" + region + "_zplus",projections[-1].GetName())
-            canvasesProj.append(canvasProj)
-            canvasesProj[-1].SetGridy()
-            #canvasesProj[-1].SetLogy()
-            canvasesProj[-1].cd() 
-	    frames[-1].Draw()
-            break
             ### 
             #projections[-1].DrawNormalized() 
             #if len(fitObjects[-1]): fitObjects[-1][0].Draw()
-            #projections[-1].Draw()
+            projections[-1].Draw()
    
             linesProj.append(ROOT.TLine(ythresholdNew,0.,
                                         ythresholdNew,0.15))
@@ -358,17 +295,15 @@ def plot(fileName,selection="pFlowAnalysisNoCollNoVtx",rebin=1,outputFileName=""
             linesProj[-1].SetLineWidth(4)
             linesProj[-1].Draw("SAME")
  
-        #canvases.append(canvas)
+        canvases.append(canvas)
         canvases.append(canvasesProj)
         histos.append(histo)
         for proj in projections: histos.append(proj)
         objects.append(fitObjects)
-        objects.append(frames)
         objects.append(lines)
         objects.append(linesProj)
-        break
 
-    #ROOT.TH1.AddDirectory(AddDirectoryStatus_)
+    ROOT.TH1.AddDirectory(AddDirectoryStatus_)
 
     for type in particleTypes:
         if not thresholdsPlus.has_key(type): continue
